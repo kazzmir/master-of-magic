9f97432952521f64d601d0db989d3ed02166c781
Dave Rafkind <dave.rafkind@gmail.com>
2011-11-16 23:42:28 -0500

    adding base slick2d stuff; pops up a window with a framerate

--- src/MasterOfMagic/src/com/rafkind/masterofmagic/Main.scala
+++ src/MasterOfMagic/src/com/rafkind/masterofmagic/Main.scala
@@ -12,13 +12,23 @@
 
 package com.rafkind.masterofmagic
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.ScalableGame;
+
 object Main {
 
   /**
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    var app = new AppGameContainer(new ScalableGame(new MasterOfMagic("hams"), 320, 200));
+    app.setDisplayMode(640, 480, false);
+    app.setSmoothDeltas(true);
+    app.setTargetFrameRate(40);
+    app.setShowFPS(true);
+    app.start();
   }
 
 }
---
+++ src/MasterOfMagic/src/com/rafkind/masterofmagic/MasterOfMagic.scala
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rafkind.masterofmagic
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+class MasterOfMagic(title:String) extends BasicGame(title) {
+  override def init(gc:GameContainer):Unit = {
+
+  }
+
+  override def update(gc:GameContainer, delta:Int):Unit = {
+  }
+
+  override def render(gc:GameContainer, graphics:Graphics):Unit = {
+    
+  }
+}
