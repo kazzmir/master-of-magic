4663a59be11b7091312c4413ec4a7ba3a9ed6f7a
Dave Rafkind <dave.rafkind@gmail.com>
2012-02-27 13:42:42 -0500

    moved the metadata editor to a seperate project

---
+++ src/Util/src/com/rafkind/masterofmagic/util/Main.scala
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rafkind.masterofmagic.util
+
+object Main {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]): Unit = {
+    TerrainMetadataEditor.main(args);
+  }
+}
