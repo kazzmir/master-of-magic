17a6dbaf6782a9e948f000156cb3129535274201
Dave Rafkind <dave.rafkind@gmail.com>
2012-08-01 12:06:41 -0400

    playing around with event code

--- src/MasterOfMagic/src/com/rafkind/masterofmagic/ui/PlayingGameState.scala
+++ src/MasterOfMagic/src/com/rafkind/masterofmagic/ui/PlayingGameState.scala
@@ -19,17 +19,17 @@ class PlayingGameState @Inject() (imageLibrarian:ImageLibrarian, mainScreen:Scre
   
   def getID() = 1;
 
-  var text:Image = null;
+  //var text:Image = null;
 
   def init(container:GameContainer, game:StateBasedGame):Unit = {
 
-    val font = imageLibrarian.getFont(FontIdentifier.HUGE);
+    /*val font = imageLibrarian.getFont(FontIdentifier.HUGE);
     val textib = new ImageBuffer(320, 50);
     val c = new Color(255, 255, 255, 255);
     val colors = Array(c, c, c, c, c, c, c, c, c, c, c, c, c, c, c, c, c, c, c, c);
 
     font.render(textib, 0, 0, colors, "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG. PACK MY BOX WITH FIVE DOZEN LIQUOR JUGS.");
-    text = textib.getImage();
+    text = textib.getImage();*/
 
     mainScreen.set(
       Component.BACKGROUND_IMAGE ->
@@ -98,15 +98,18 @@ class PlayingGameState @Inject() (imageLibrarian:ImageLibrarian, mainScreen:Scre
           ), Alignment.HORIZONTAL
         )
       );
-
     
     currentScreen = mainScreen;
   }
 
   def render(container:GameContainer, game:StateBasedGame, graphics:Graphics):Unit = {
     currentScreen.render(graphics);
-    text.draw(0, 0);
+    //text.draw(0, 0);
   }
 
   def update(container:GameContainer, game:StateBasedGame, delta:Int):Unit = { }
+
+  override def mouseMoved(oldX:Int, oldY:Int, newX:Int, newY:Int):Unit = {
+    println(oldX + ", " + oldY + " -> " + newX + ", " + newY);
+  }
 }
