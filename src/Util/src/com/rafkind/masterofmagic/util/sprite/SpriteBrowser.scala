3125afdc4bafa99e90c0152479ed53e31cf2f3b3
Dave Rafkind <dave.rafkind@gmail.com>
2012-11-15 15:20:46 -0500

    getting other listboxes to have something valid

--- src/Util/src/com/rafkind/masterofmagic/util/sprite/SpriteBrowser.scala
+++ src/Util/src/com/rafkind/masterofmagic/util/sprite/SpriteBrowser.scala
@@ -8,7 +8,9 @@ package com.rafkind.masterofmagic.util.sprite
 import com.rafkind.masterofmagic.system.Data
 import com.rafkind.masterofmagic.util.LbxReader
 import com.rafkind.masterofmagic.util.OriginalGameAsset
+import com.rafkind.masterofmagic.util.SpriteReader
 import java.awt.GraphicsEnvironment
+import javax.swing.AbstractListModel
 import javax.swing.Box
 import javax.swing.JFrame
 import javax.swing.JList
@@ -29,29 +31,95 @@ object SpriteBrowser {
     var frame = new JFrame("Sprite Browser");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     val mainBox = Box.createHorizontalBox();
-    val lbxes = new JList(OriginalGameAsset.values);      
+    val lbxModel = new AbstractListModel[OriginalGameAsset]() {
+      override def getElementAt(index:Int) =
+        OriginalGameAsset.values(index);
+      override def getSize() =
+        OriginalGameAsset.values.size;
+    };
+    val lbxes = new JList(lbxModel);      
     mainBox.add(new JScrollPane(lbxes));
-    val subfiles = new JList(Array("A", "B", "C"));    
+    val subFileModel = 
+      new AbstractListModel[Int]() {
+      var currentSize:Int = 0;
+      
+      val self = this;
+      
+      lbxes.addListSelectionListener(new ListSelectionListener(){
+        override def valueChanged(event:ListSelectionEvent):Unit = {
+          var newSize:Int = currentSize;
+          
+          if (!event.getValueIsAdjusting()) {
+            val selected = lbxes.getSelectedValue();
+            if (selected.fileName.endsWith(".LBX")) {
+              val reader = new LbxReader(Data.originalDataPath(selected.fileName));
+              val metaData = reader.metaData;
+              newSize = metaData.subfileCount
+            } else {
+              newSize = 0;
+            }
+            if (newSize != currentSize) {
+              if (newSize > currentSize) {
+                fireIntervalAdded(self, currentSize, newSize);
+              } else {
+                fireIntervalRemoved(self, newSize, currentSize);
+              }
+              currentSize = newSize;
+            }
+          }
+        }
+      });      
+      override def getElementAt(index:Int) = index;
+      override def getSize() = currentSize;
+    }
+    val subfiles = new JList(subFileModel);    
     mainBox.add(Box.createHorizontalStrut(5));
-    mainBox.add(subfiles);
-    val groupIndexes = new JList(Array("A", "B", "C"));
+    mainBox.add(new JScrollPane(subfiles));
+    
+    val groupIndexModel = new AbstractListModel[Int](){
+      var currentSize:Int = 0;
+      
+      val self = this;
+      
+      subfiles.addListSelectionListener(new ListSelectionListener(){
+        override def valueChanged(event:ListSelectionEvent):Unit = {
+          var newSize:Int = currentSize;
+          
+          if (!event.getValueIsAdjusting()) {
+            val selected = lbxes.getSelectedValue();            
+            if (selected.fileName.endsWith(".LBX")) {              
+              var subSelected = subfiles.getSelectedIndex();
+              if (subSelected >= 0) {
+                val reader = new LbxReader(Data.originalDataPath(selected.fileName));
+                val header = SpriteReader.readHeader(reader, subfiles.getSelectedIndex());
+                newSize = header.bitmapCount;
+              } else {
+                newSize = 0;
+              }
+            } else {
+              newSize = 0;
+            }
+            if (newSize != currentSize) {
+              if (newSize > currentSize) {
+                fireIntervalAdded(self, currentSize, newSize);
+              } else {
+                fireIntervalRemoved(self, newSize, currentSize);
+              }
+              currentSize = newSize;
+            }
+          }
+        }
+      });      
+      override def getElementAt(index:Int) = index;
+      override def getSize() = currentSize;
+    };
+    
+    val groupIndexes = new JList(groupIndexModel);
     mainBox.add(Box.createHorizontalStrut(5));
-    mainBox.add(groupIndexes);
+    mainBox.add(new JScrollPane(groupIndexes));
     val sprites = new JList(Array("A", "B", "C"));
     mainBox.add(Box.createHorizontalStrut(5));
     mainBox.add(sprites);
-    
-    lbxes.addListSelectionListener(new ListSelectionListener(){
-      override def valueChanged(event:ListSelectionEvent):Unit = {
-        if (!event.getValueIsAdjusting()) {
-          val selected = lbxes.getSelectedValue();
-          if (selected.fileName.endsWith(".LBX")) {
-            /*val reader = new LbxReader(Data.)
-            subfiles.setModel(x$1);*/
-          }
-        }
-      }
-    });
     
     frame.getContentPane().add(mainBox);
     frame.pack();
