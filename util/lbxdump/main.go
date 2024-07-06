1b7cd211fb0bc9afcffcfbfedd0d1df0eecfa0ea
Jon Rafkind <jon@rafkind.com>
2024-07-06 14:35:03 -0700

    read lbx header

--- util/lbxdump/main.go
+++ util/lbxdump/main.go
@@ -4,11 +4,64 @@ import (
     "os"
     "io"
     "fmt"
+    "encoding/binary"
     "strings"
     "archive/zip"
 )
 
+const LbxSignature = 0x0000fead
+
+func readUint16(reader io.Reader) (uint16, error) {
+    var value uint16
+    err := binary.Read(reader, binary.LittleEndian, &value)
+    return value, err
+}
+
+func readUint32(reader io.Reader) (uint32, error) {
+    var value uint32
+    err := binary.Read(reader, binary.LittleEndian, &value)
+    return value, err
+}
+
+func readInt32(reader io.Reader) (int32, error) {
+    v, err := readUint32(reader)
+    return int32(v), err
+}
+
 func dumpLbx(reader io.Reader) error {
+    numFiles, err := readUint16(reader)
+    if err != nil {
+        return err
+    }
+
+    signature, err := readUint32(reader)
+    if err != nil {
+        return err
+    }
+
+    fmt.Printf("Number of files: %v\n", numFiles)
+    fmt.Printf("Signature: 0x%x\n", signature)
+
+    if signature != LbxSignature {
+        return fmt.Errorf("Invalid lbx signature, was 0x%x but expected 0x%x\n", signature, LbxSignature)
+    }
+
+    version, err := readUint16(reader)
+    if err != nil {
+        return err
+    }
+
+    fmt.Printf("Version: %v\n", version)
+
+    for i := 0; i < int(numFiles); i++ {
+        offset, err := readUint32(reader)
+        if err != nil {
+            return err
+        }
+
+        fmt.Printf("Offset %v: 0x%x\n", i, offset)
+    }
+
     return nil
 }
 
