969effddb7f645c0aaf84d3fe6bff0c91085eccb
Jon Rafkind <jon@rafkind.com>
2024-07-06 14:27:30 -0700

    open zip file, search for lbx entry

--- util/lbxdump/main.go
+++ util/lbxdump/main.go
@@ -2,12 +2,79 @@ package main
 
 import (
     "os"
+    "io"
     "fmt"
+    "strings"
+    "archive/zip"
 )
+
+func dumpLbx(reader io.Reader) error {
+    return nil
+}
 
 func main(){
     if len(os.Args) < 2 {
         fmt.Println("Give an lbx file, or a zip file and the name of an lbx file inside it")
         return
     }
+
+    if len(os.Args) == 2 {
+        fmt.Printf("Opening %v as an lbx file\n", os.Args[1])
+    } else if len(os.Args) == 3 {
+        zipFile, err := zip.OpenReader(os.Args[1])
+        if err != nil {
+            fmt.Printf("Error opening zip file: %s\n", err)
+            return
+        }
+        defer zipFile.Close()
+
+        searchName := os.Args[2]
+
+        var matches []string
+        for _, file := range zipFile.File {
+            // fmt.Printf("Entry: %s\n", file.Name)
+
+            lower := strings.ToLower(file.Name)
+            check := strings.ToLower(searchName)
+
+            if strings.Contains(lower, check) {
+                matches = append(matches, file.Name)
+            }
+        }
+
+        if len(matches) == 0 {
+            fmt.Printf("No such entry with name '%v'\n", searchName)
+            return
+        }
+
+        if len(matches) > 1 {
+            fmt.Printf("More than one match found for '%v'\n", searchName)
+            for _, name := range matches {
+                fmt.Printf("  %v\n", name)
+            }
+            return
+        }
+
+        match := matches[0]
+        for _, file := range zipFile.File {
+            if file.Name == match {
+                opened, err := file.Open()
+                if err != nil {
+                    fmt.Printf("Unable to open entry %v: %v\n", file.Name, err)
+                } else {
+                    fmt.Printf("Dumping %v\n", file.Name)
+                    err := dumpLbx(opened)
+                    if err != nil {
+                        fmt.Printf("Error dumping lbx file: %v\n", err)
+                    }
+                    opened.Close()
+                }
+            }
+        }
+
+    } else {
+        fmt.Println("Too many arguments")
+        return
+    }
+
 }
