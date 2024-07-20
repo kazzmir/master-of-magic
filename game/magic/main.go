3c0132bf35203ba06b3a5425375fb1c25e17ee55
Jon Rafkind <jon@rafkind.com>
2024-07-19 23:21:43 -0700

    make window and draw a background color

--- game/magic/main.go
+++ game/magic/main.go
@@ -3,11 +3,28 @@ package main
 import (
     "log"
 
+    "image/color"
+
     "github.com/hajimehoshi/ebiten/v2"
 )
 
 const ScreenWidth = 1024
 const ScreenHeight = 768
+
+type MagicGame struct {
+}
+
+func (game *MagicGame) Update() error {
+    return nil
+}
+
+func (game *MagicGame) Layout(outsideWidth int, outsideHeight int) (int, int) {
+    return ScreenWidth, ScreenHeight
+}
+
+func (game *MagicGame) Draw(screen *ebiten.Image) {
+    screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
+}
 
 func main() {
     log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
@@ -15,4 +32,12 @@ func main() {
     ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
     ebiten.SetWindowTitle("magic")
     ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+    var game MagicGame
+
+    err := ebiten.RunGame(&game)
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
+
 }
