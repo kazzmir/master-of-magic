939e98a64c249683c6257270c9cac8adca14547c
Jon Rafkind <jon@rafkind.com>
2024-07-19 23:34:01 -0700

    load lbx argument

--- util/lbxviewer/main.go
+++ util/lbxviewer/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
     "log"
+    "os"
 
     "image/color"
+
+    "github.com/kazzmir/master-of-magic/lib/lbx"
 
     "github.com/hajimehoshi/ebiten/v2"
     "github.com/hajimehoshi/ebiten/v2/inpututil"
@@ -13,6 +16,7 @@ const ScreenWidth = 1024
 const ScreenHeight = 768
 
 type Viewer struct {
+    Lbx *lbx.LbxFile
 }
 
 func (viewer *Viewer) Update() error {
@@ -36,14 +40,44 @@ func (viewer *Viewer) Draw(screen *ebiten.Image) {
     screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
 }
 
+func MakeViewer(lbxFile *lbx.LbxFile) Viewer {
+    return Viewer{
+        Lbx: lbxFile,
+    }
+}
+
 func main() {
     log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
+
+    if len(os.Args) < 2 {
+        log.Printf("Give an lbx file to view")
+        return
+    }
+
+    file := os.Args[1]
 
     ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
     ebiten.SetWindowTitle("lbx viewer")
     ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-    var viewer Viewer
+    var lbxFile lbx.LbxFile
+
+    func(){
+        open, err := os.Open(file)
+        if err != nil {
+            log.Printf("Error: %v", err)
+            return
+        }
+        defer open.Close()
+        lbxFile, err = lbx.ReadLbx(open)
+        if err != nil {
+            log.Printf("Error: %v\n", err)
+            return
+        }
+        log.Printf("Loaded lbx file: %v\n", file)
+    }()
+
+    viewer := MakeViewer(&lbxFile)
 
     err := ebiten.RunGame(&viewer)
     if err != nil {
