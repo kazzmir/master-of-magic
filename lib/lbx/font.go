3874abae4b402d6ca4cfa60c11c2326f4e902fba
Jon Rafkind <jon@rafkind.com>
2024-07-21 00:15:02 -0700

    add font comments

---
+++ lib/lbx/font.go
@@ -0,0 +1,37 @@
+package lbx
+
+/*
+https://steamcommunity.com/app/1146370/discussions/0/5879990700799122827/
+
+char = 1 byte
+unsigned = 2 bytes
+little endian
+
+typedef struct
+{
+	FONT_HEADER	Hdr_Space;
+	unsigned	Font_Heights[8];
+	unsigned	Horz_Spacings[8];
+	unsigned	Vert_Spacings[8];
+	char		Glyph_Widths[8][96];
+	unsigned	Glyph_Offsets[8][96]; // should start at offset 0x16a
+	char		Glyph_Data[15066]; // first glyph should be at 0x49a
+} FONT_ENTRY;
+
+// all 0's when read out of the file
+typedef struct
+{
+	char		Current_Colors[16];
+	unsigned	Font_Height;
+	char		Outline_Style;
+	char		Color_Index;
+	char		Colors_1[16];
+	char		Colors_2[16];
+	char		Colors_3[16];
+	unsigned	Line_Height;
+	unsigned	Vert_Spacing;
+	unsigned	Horz_Spacing;
+	char		Glyph_Widths[96];
+	unsigned	Glyph_Offsets[96];
+} FONT_HEADER;
+ */
