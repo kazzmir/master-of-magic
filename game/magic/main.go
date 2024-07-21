2fb082ef6a91683398bcf1bc7d901817ad26b8dd
Jon Rafkind <jon@rafkind.com>
2024-07-20 22:24:11 -0700

    render new game background

--- game/magic/main.go
+++ game/magic/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
     "log"
-
     "image/color"
+    "sync"
 
     "github.com/kazzmir/master-of-magic/lib/lbx"
 
@@ -14,8 +14,52 @@ import (
 const ScreenWidth = 1024
 const ScreenHeight = 768
 
+func stretchImage(screen *ebiten.Image, sprite *ebiten.Image){
+    var options ebiten.DrawImageOptions
+    options.GeoM.Scale(float64(ScreenWidth) / float64(sprite.Bounds().Dx()), float64(ScreenHeight) / float64(sprite.Bounds().Dy()))
+    screen.DrawImage(sprite, &options)
+}
+
+type NewGameScreen struct {
+    LbxFile *lbx.LbxFile
+    Background *ebiten.Image
+    loaded sync.Once
+}
+
+func (newGameScreen *NewGameScreen) Load(cache *lbx.LbxCache) error {
+    var outError error = nil
+
+    newGameScreen.loaded.Do(func() {
+        newGameLbx, err := cache.GetLbxFile("magic-data/NEWGAME.LBX")
+        if err != nil {
+            log.Printf("Unable to load NEWGAME.LBX: %v", err)
+            outError = err
+            return
+        }
+
+        background, err := newGameLbx.ReadImages(0)
+        if err != nil {
+            log.Printf("Unable to read background image from NEWGAME.LBX: %v", err)
+            outError = err
+            return
+        }
+
+        newGameScreen.Background = ebiten.NewImageFromImage(background[0])
+    })
+
+    return outError
+}
+
+func (newGameScreen *NewGameScreen) Draw(screen *ebiten.Image) {
+    if newGameScreen.Background != nil {
+        stretchImage(screen, newGameScreen.Background)
+    }
+}
+
 type MagicGame struct {
     LbxCache *lbx.LbxCache
+
+    NewGameScreen NewGameScreen
 }
 
 func NewMagicGame() *MagicGame {
@@ -34,6 +78,8 @@ func (game *MagicGame) Update() error {
         }
     }
 
+    game.NewGameScreen.Load(game.LbxCache)
+
     return nil
 }
 
@@ -43,6 +89,8 @@ func (game *MagicGame) Layout(outsideWidth int, outsideHeight int) (int, int) {
 
 func (game *MagicGame) Draw(screen *ebiten.Image) {
     screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
+
+    game.NewGameScreen.Draw(screen)
 }
 
 func main() {
