028e8fc05a6f93efb76b2875519e169d44e1bb7b
Jon Rafkind <jon@rafkind.com>
2024-08-01 00:12:24 -0700

    lbxviewer: add tile view of all images (#6)
    * show all lbx images as tiles
    
    * show highlight around tiles
    
    * show highlighted tile when pressing enter
    
    * cleanup, and show image dimensions
    
    * fix animation
    
    * make tile sizes constants
    
    * add fixme
    
    * remove

--- util/lbxviewer/main.go
+++ util/lbxviewer/main.go
@@ -4,7 +4,7 @@ import (
     "log"
     "os"
     "fmt"
-    // "sync"
+    "sync"
     "math"
     "bytes"
     _ "embed"
@@ -14,6 +14,7 @@ import (
     "github.com/kazzmir/master-of-magic/lib/lbx"
 
     "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/vector"
     "github.com/hajimehoshi/ebiten/v2/inpututil"
     "github.com/hajimehoshi/ebiten/v2/text/v2"
 )
@@ -28,34 +29,44 @@ func LoadFont() (*text.GoTextFaceSource, error) {
     return text.NewGoTextFaceSource(bytes.NewReader(FuturaTTF))
 }
 
+type LbxImages struct {
+    Images []*ebiten.Image
+    Load sync.Once
+    Loaded bool
+    Lock sync.Mutex
+}
+
+func (loader *LbxImages) IsLoaded() bool {
+    loader.Lock.Lock()
+    defer loader.Lock.Unlock()
+    return loader.Loaded
+}
+
+type ViewerState int
+
+const (
+    ViewStateTiles ViewerState = iota
+    ViewStateImage
+)
+
 type Viewer struct {
     Lbx *lbx.LbxFile
-    Images []*ebiten.Image
+    Images []*LbxImages
     Scale float64
     CurrentImage int
-    LbxEntry int
+    CurrentTile int
+    State ViewerState
     Font *text.GoTextFaceSource
     AnimationFrame int
     AnimationCount int
 }
 
-func (viewer *Viewer) LoadImages() {
-    rawImages, err := viewer.Lbx.ReadImages(viewer.LbxEntry)
-    if err != nil {
-        log.Printf("Unable to load images: %v", err)
-        return
-    }
-    var images []*ebiten.Image
-    for _, rawImage := range rawImages {
-        images = append(images, ebiten.NewImageFromImage(rawImage))
-    }
-    viewer.Images = images
-    viewer.CurrentImage = 0
+const TileWidth = 50
+const TileHeight = 50
 
-    if len(images) > 0 {
-        bounds := viewer.Images[viewer.CurrentImage].Bounds()
-        viewer.Scale = 200.0 / math.Max(float64(bounds.Dx()), float64(bounds.Dy()))
-    }
+func tilesPerRow() int {
+    width := ScreenWidth - 1
+    return width / TileWidth
 }
 
 func (viewer *Viewer) Update() error {
@@ -64,25 +75,33 @@ func (viewer *Viewer) Update() error {
 
     const AnimationSpeed = 30
 
-    control_pressed := false
+    // control_pressed := false
 
     scaleAmount := 0.06
 
     for _, key := range keys {
         switch key {
             case ebiten.KeyUp:
-                viewer.Scale *= 1 + scaleAmount
-            case ebiten.KeyDown:
-                viewer.Scale *= 1 - scaleAmount
-                if viewer.Scale < 1 {
-                    viewer.Scale = 1
+                if viewer.State == ViewStateImage {
+                    viewer.Scale *= 1 + scaleAmount
                 }
+            case ebiten.KeyDown:
+                if viewer.State == ViewStateImage {
+                    viewer.Scale *= 1 - scaleAmount
+                    if viewer.Scale < 1 {
+                        viewer.Scale = 1
+                    }
+                }
+                /*
             case ebiten.KeyControlLeft:
                 control_pressed = true
+                */
             case ebiten.KeySpace:
-                if len(viewer.Images) > 0 {
-                    bounds := viewer.Images[viewer.CurrentImage].Bounds()
-                    viewer.Scale = 200.0 / math.Max(float64(bounds.Dx()), float64(bounds.Dy()))
+                if viewer.State == ViewStateImage {
+                    if len(viewer.Images[viewer.CurrentTile].Images) > 0 {
+                        bounds := viewer.Images[viewer.CurrentTile].Images[viewer.CurrentImage].Bounds()
+                        viewer.Scale = 200.0 / math.Max(float64(bounds.Dx()), float64(bounds.Dy()))
+                    }
                 }
         }
 
@@ -93,16 +112,61 @@ func (viewer *Viewer) Update() error {
 
     for _, key := range keys {
         switch key {
+            case ebiten.KeyEnter:
+                if viewer.State == ViewStateTiles {
+                    viewer.State = ViewStateImage
+                } else {
+                    viewer.State = ViewStateTiles
+                }
+
             case ebiten.KeyLeft:
-                viewer.CurrentImage -= 1
-                if viewer.CurrentImage < 0 {
-                    viewer.CurrentImage = len(viewer.Images) - 1
+                switch viewer.State {
+                    case ViewStateTiles:
+                        if viewer.CurrentTile > 0 {
+                            viewer.CurrentTile -= 1
+                            viewer.CurrentImage = 0
+                        }
+                    case ViewStateImage:
+                        viewer.CurrentImage -= 1
+                        if viewer.CurrentImage < 0 {
+                            viewer.CurrentImage = len(viewer.Images[viewer.CurrentTile].Images) - 1
+                        }
                 }
+
             case ebiten.KeyRight:
-                viewer.CurrentImage += 1
-                if viewer.CurrentImage >= len(viewer.Images) {
-                    viewer.CurrentImage = 0
+                switch viewer.State {
+                    case ViewStateTiles:
+                        if viewer.CurrentTile < len(viewer.Images) - 1 {
+                            viewer.CurrentTile += 1
+                            viewer.CurrentImage = 0
+                        }
+                    case ViewStateImage:
+                        viewer.CurrentImage += 1
+                        if viewer.CurrentImage >= len(viewer.Images[viewer.CurrentTile].Images) {
+                            viewer.CurrentImage = 0
+                        }
                 }
+
+            case ebiten.KeyUp:
+                switch viewer.State {
+                    case ViewStateTiles:
+                        position := viewer.CurrentTile - tilesPerRow()
+                        if position >= 0 {
+                            viewer.CurrentTile = position
+                            viewer.CurrentImage = 0
+                        }
+                }
+
+            case ebiten.KeyDown:
+                switch viewer.State {
+                    case ViewStateTiles:
+                        position := viewer.CurrentTile + tilesPerRow()
+                        if position < len(viewer.Images) {
+                            viewer.CurrentTile = position
+                            viewer.CurrentImage = 0
+                        }
+                }
+
             case ebiten.KeyA:
                 if viewer.AnimationFrame == -1 {
                     viewer.AnimationFrame = 0
@@ -110,35 +174,7 @@ func (viewer *Viewer) Update() error {
                 } else {
                     viewer.AnimationFrame = -1
                 }
-            case ebiten.KeyPageUp:
-                if viewer.AnimationFrame != -1 {
-                    viewer.AnimationFrame = 0
-                }
-                amount := 1
-                if control_pressed {
-                    amount = 10
-                }
-                viewer.LbxEntry -= amount
-                if viewer.LbxEntry < 0 {
-                    viewer.LbxEntry = viewer.Lbx.TotalEntries() - 1
-                }
 
-                if viewer.LbxEntry >= 0 {
-                    viewer.LoadImages()
-                }
-            case ebiten.KeyPageDown:
-                if viewer.AnimationFrame != -1 {
-                    viewer.AnimationFrame = 0
-                }
-                amount := 1
-                if control_pressed {
-                    amount = 10
-                }
-                viewer.LbxEntry += amount
-                if viewer.LbxEntry >= viewer.Lbx.TotalEntries() {
-                    viewer.LbxEntry = 0
-                }
-                viewer.LoadImages()
             case ebiten.KeyEscape, ebiten.KeyCapsLock:
                 return ebiten.Termination
         }
@@ -149,7 +185,7 @@ func (viewer *Viewer) Update() error {
             viewer.AnimationCount -= 1
         } else {
             viewer.AnimationFrame += 1
-            if viewer.AnimationFrame >= len(viewer.Images) {
+            if viewer.AnimationFrame >= len(viewer.Images[viewer.CurrentTile].Images) {
                 viewer.AnimationFrame = 0
             }
             viewer.AnimationCount = AnimationSpeed
@@ -163,6 +199,15 @@ func (viewer *Viewer) Layout(outsideWidth int, outsideHeight int) (int, int) {
     return ScreenWidth, ScreenHeight
 }
 
+func aspectScale(width, height, maxWidth, maxHeight int) (float64, float64) {
+    scaleX := float64(maxWidth) / float64(width)
+    scaleY := float64(maxHeight) / float64(height)
+    if scaleX < scaleY {
+        return scaleX, scaleX
+    }
+    return scaleY, scaleY
+}
+
 func (viewer *Viewer) Draw(screen *ebiten.Image) {
     screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
 
@@ -171,30 +216,74 @@ func (viewer *Viewer) Draw(screen *ebiten.Image) {
     op := &text.DrawOptions{}
     op.GeoM.Translate(1, 1)
     op.ColorScale.ScaleWithColor(color.White)
-    text.Draw(screen, fmt.Sprintf("Lbx entry: %v/%v", viewer.LbxEntry, viewer.Lbx.TotalEntries() - 1), face, op)
+    text.Draw(screen, fmt.Sprintf("Lbx entry: %v/%v", viewer.CurrentTile, viewer.Lbx.TotalEntries() - 1), face, op)
     op.GeoM.Translate(1, 20)
     if viewer.AnimationFrame != -1 {
-        text.Draw(screen, fmt.Sprintf("Animation : %v/%v", viewer.AnimationFrame+1, len(viewer.Images)), face, op)
+        text.Draw(screen, fmt.Sprintf("Animation : %v/%v", viewer.AnimationFrame+1, len(viewer.Images[viewer.CurrentTile].Images)), face, op)
     } else {
-        text.Draw(screen, fmt.Sprintf("Image: %v/%v", viewer.CurrentImage+1, len(viewer.Images)), face, op)
+        text.Draw(screen, fmt.Sprintf("Image: %v/%v", viewer.CurrentImage+1, len(viewer.Images[viewer.CurrentTile].Images)), face, op)
     }
     op.GeoM.Translate(0, 20)
     text.Draw(screen, fmt.Sprintf("Scale: %.2f", viewer.Scale), face, op)
+    op.GeoM.Translate(0, 20)
+    if len(viewer.Images[viewer.CurrentTile].Images) > 0 {
+        img := viewer.Images[viewer.CurrentTile].Images[viewer.CurrentImage]
+        text.Draw(screen, fmt.Sprintf("Dimensions: %v x %v", img.Bounds().Dx(), img.Bounds().Dy()), face, op)
+    }
 
-    middleX := ScreenWidth / 2
-    middleY := ScreenHeight / 2
+    startX := 1
+    startY := 100
 
-    if len(viewer.Images) > 0 {
-        var options ebiten.DrawImageOptions
-        useImage := viewer.Images[viewer.CurrentImage]
-        if viewer.AnimationFrame != -1 {
-            useImage = viewer.Images[viewer.AnimationFrame]
+    x := startX
+    y := startY
+
+    // FIXME: handle the case when there are more images than can fit on the screen
+    for i, image := range viewer.Images {
+        if image.IsLoaded() && len(image.Images) > 0 {
+            var options ebiten.DrawImageOptions
+
+            draw := image.Images[0]
+
+            scaleX, scaleY := aspectScale(draw.Bounds().Dx(), draw.Bounds().Dy(), TileWidth, TileHeight)
+
+            options.GeoM.Scale(scaleX, scaleY)
+            options.GeoM.Translate(float64(x), float64(y))
+            screen.DrawImage(draw, &options)
+            /*
+            text.Draw(screen, fmt.Sprintf("%v", i), face, &text.DrawOptions{
+                GeoM: ebiten.GeoM.Translate(float64(x), float64(y)),
+            })
+            */
         }
-        bounds := useImage.Bounds()
-        options.GeoM.Translate(float64(-bounds.Dx()) / 2.0, float64(-bounds.Dy()) / 2.0)
-        options.GeoM.Scale(viewer.Scale, viewer.Scale)
-        options.GeoM.Translate(float64(middleX), float64(middleY))
-        screen.DrawImage(useImage, &options)
+
+        if i == viewer.CurrentTile {
+            vector.StrokeRect(screen, float32(x), float32(y), float32(TileWidth), float32(TileHeight), 1.5, color.White, true)
+        }
+
+        x += TileWidth
+        if x + TileWidth >= ScreenWidth {
+            x = 1
+            y += TileHeight
+        }
+    }
+
+    if viewer.State == ViewStateImage {
+        if len(viewer.Images[viewer.CurrentTile].Images) > 0 {
+            vector.DrawFilledRect(screen, 0, float32(startY), float32(ScreenWidth), float32(ScreenHeight - startY), color.RGBA{0, 0, 0, 64}, false)
+            middleX := ScreenWidth / 2
+            middleY := ScreenHeight / 2
+
+            var options ebiten.DrawImageOptions
+            useImage := viewer.Images[viewer.CurrentTile].Images[viewer.CurrentImage]
+            if viewer.AnimationFrame != -1 && viewer.AnimationFrame < len(viewer.Images[viewer.CurrentTile].Images) {
+                useImage = viewer.Images[viewer.CurrentTile].Images[viewer.AnimationFrame]
+            }
+            bounds := useImage.Bounds()
+            options.GeoM.Translate(float64(-bounds.Dx()) / 2.0, float64(-bounds.Dy()) / 2.0)
+            options.GeoM.Scale(viewer.Scale, viewer.Scale)
+            options.GeoM.Translate(float64(middleX), float64(middleY))
+            screen.DrawImage(useImage, &options)
+        }
     }
 }
 
@@ -209,12 +298,35 @@ func MakeViewer(lbxFile *lbx.LbxFile) (*Viewer, error) {
         Scale: 1,
         Font: font,
         CurrentImage: 0,
-        LbxEntry: 0,
         AnimationFrame: -1,
         AnimationCount: 0,
+        State: ViewStateTiles,
     }
 
-    viewer.LoadImages()
+    for i := 0; i < lbxFile.TotalEntries(); i++ {
+        loader := &LbxImages{}
+        viewer.Images = append(viewer.Images, loader)
+
+        go func(){
+            loader.Load.Do(func(){
+                rawImages, err := lbxFile.ReadImages(i)
+                if err != nil {
+                    log.Printf("Unable to load images: %v", err)
+                    return
+                }
+                var images []*ebiten.Image
+                for _, rawImage := range rawImages {
+                    images = append(images, ebiten.NewImageFromImage(rawImage))
+                }
+                loader.Images = images
+
+                loader.Lock.Lock()
+                loader.Loaded = true
+                loader.Lock.Unlock()
+            })
+        }()
+    }
+
     return viewer, nil
 }
 
