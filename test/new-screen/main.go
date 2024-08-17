2f71a94d1bf28687248e731e26b23b1e82c0edb8
Jon Rafkind <jon@rafkind.com>
2024-08-17 13:56:45 -0700

    add tests for running the new game and new wizard screens (#7)
    * start test for new screen
    
    * create engine
    
    * run new game screen
    
    * print settings
    
    * add new wizard test
    
    * dont set the custom wizard in the new wizard constructor

---
+++ test/new-screen/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "log"
+
+    "github.com/kazzmir/master-of-magic/lib/lbx"
+    "github.com/kazzmir/master-of-magic/game/magic/setup"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Engine struct {
+    LbxCache *lbx.LbxCache
+    NewGameScreen *setup.NewGameScreen
+}
+
+func NewEngine() (*Engine, error) {
+    cache := lbx.MakeLbxCache("magic-data")
+
+    screen := setup.MakeNewGameScreen()
+    err := screen.Load(cache)
+    if err != nil {
+        return nil, err
+    }
+
+    screen.Activate()
+
+    return &Engine{
+        LbxCache: cache,
+        NewGameScreen: screen,
+    }, nil
+}
+
+func (engine *Engine) Update() error {
+
+    keys := make([]ebiten.Key, 0)
+    keys = inpututil.AppendJustPressedKeys(keys)
+
+    for _, key := range keys {
+        if key == ebiten.KeyEscape || key == ebiten.KeyCapsLock {
+            return ebiten.Termination
+        }
+    }
+
+    switch engine.NewGameScreen.Update() {
+        case setup.NewGameStateRunning:
+        case setup.NewGameStateOk:
+            log.Printf("Creating new game with settings: %+v", engine.NewGameScreen.Settings)
+            return ebiten.Termination
+        case setup.NewGameStateCancel:
+            log.Printf("Cancel")
+            return ebiten.Termination
+    }
+
+    return nil
+}
+
+func (engine *Engine) Draw(screen *ebiten.Image) {
+    engine.NewGameScreen.Draw(screen)
+}
+
+func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return data.ScreenWidth, data.ScreenHeight
+}
+
+func main(){
+
+    log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
+
+    ebiten.SetWindowSize(data.ScreenWidth * 5, data.ScreenHeight * 5)
+    ebiten.SetWindowTitle("new screen")
+    ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+    engine, err := NewEngine()
+
+    if err != nil {
+        log.Printf("Error: unable to load engine: %v", err)
+        return
+    }
+
+    err = ebiten.RunGame(engine)
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
+
+}
