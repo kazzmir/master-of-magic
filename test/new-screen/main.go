01c29c96cb2a0cad55b3d791a10fcd4c0fa81993
Jon Rafkind <jon@rafkind.com>
2024-08-17 13:41:53 -0700

    create engine

--- test/new-screen/main.go
+++ test/new-screen/main.go
@@ -1,4 +1,64 @@
 package main
 
+import (
+    "log"
+
+    _ "github.com/kazzmir/master-of-magic/lib/lbx"
+    _ "github.com/kazzmir/master-of-magic/game/magic/setup"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Engine struct {
+}
+
+func NewEngine() (*Engine, error) {
+    return &Engine{
+    }, nil
+}
+
+func (engine *Engine) Update() error {
+
+    keys := make([]ebiten.Key, 0)
+    keys = inpututil.AppendJustPressedKeys(keys)
+
+    for _, key := range keys {
+        if key == ebiten.KeyEscape || key == ebiten.KeyCapsLock {
+            return ebiten.Termination
+        }
+    }
+
+
+    return nil
+}
+
+func (engine *Engine) Draw(screen *ebiten.Image) {
+}
+
+func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return data.ScreenWidth, data.ScreenHeight
+}
+
 func main(){
+
+    log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
+
+    ebiten.SetWindowSize(data.ScreenWidth * 5, data.ScreenHeight * 5)
+    ebiten.SetWindowTitle("new screen")
+    ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+    engine, err := NewEngine()
+
+    if err != nil {
+        log.Printf("Error: unable to load engine: %v", err)
+        return
+    }
+
+    err = ebiten.RunGame(engine)
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
+
 }
