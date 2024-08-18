2d1a78725d1eacd0ca45c516d6ec2597ea1a8c45
Jon Rafkind <jon@rafkind.com>
2024-08-17 19:19:33 -0700

    add more units

--- game/magic/units/unit.go
+++ game/magic/units/unit.go
@@ -5,12 +5,16 @@ type Race int
 
 const (
     RaceLizard Race = iota
+    RaceNomad
 )
 
 type Unit struct {
     LbxFile string
     Index int
     Race Race
+    // FIXME: add construction cost, building requirements to build this unit
+    //  upkeep cost, how many figures appear in the battlefield, movement speed,
+    //  attack power, ranged attack, defense, magic resistance, hit points, special power
 }
 
 var LizardSpearmen Unit = Unit{
@@ -43,7 +47,7 @@ var LizardShamans Unit = Unit{
     Race: RaceLizard,
 }
 
-var Settlers Unit = Unit{
+var LizardSettlers Unit = Unit{
     LbxFile: "units2.lbx",
     Index: 5,
     Race: RaceLizard,
@@ -53,6 +57,64 @@ var DragonTurtle Unit = Unit{
     LbxFile: "units2.lbx",
     Index: 6,
     Race: RaceLizard,
+}
+
+var NomadSpearmen Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 7,
+    Race: RaceNomad,
+}
+
+var NomadSwordsmen Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 8,
+    Race: RaceNomad,
+}
+
+var NomadBowmen Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 9,
+    Race: RaceNomad,
+}
+
+var NomadPriest Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 10,
+    Race: RaceNomad,
+}
+
+// what is units2.lbx index 11?
+// its some nomad unit holding a sword or something
+
+var NomadSettlers Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 12,
+    Race: RaceNomad,
+}
+
+var NomadHorsebowemen Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 13,
+    Race: RaceNomad,
+}
+
+var NomadPikemen Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 14,
+    Race: RaceNomad,
+}
+
+var NomadRangers Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 15,
+    Race: RaceNomad,
+}
+
+var Griffin Unit = Unit{
+    LbxFile: "units2.lbx",
+    Index: 16,
+    // maybe race magical?
+    Race: RaceNomad,
 }
 
 var GiantBat Unit = Unit{
