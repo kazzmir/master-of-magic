0192b2f76f5d42efd261a4b2e7be8f9f50806825
Jon Rafkind <jon@rafkind.com>
2024-08-18 22:59:54 -0700

    move image cache to its own file

--- game/magic/game/game.go
+++ game/magic/game/game.go
@@ -1,9 +1,7 @@
 package game
 
 import (
-    "fmt"
     "image/color"
-    "strings"
     "log"
 
     "github.com/kazzmir/master-of-magic/game/magic/setup"
@@ -13,52 +11,6 @@ import (
     "github.com/kazzmir/master-of-magic/lib/font"
     "github.com/hajimehoshi/ebiten/v2"
 )
-
-type ImageCache struct {
-    LbxCache *lbx.LbxCache
-    Cache map[string][]*ebiten.Image
-}
-
-func (cache *ImageCache) GetImages(lbxPath string, index int) ([]*ebiten.Image, error) {
-    lbxPath = strings.ToLower(lbxPath)
-    key := fmt.Sprintf("%s:%d", lbxPath, index)
-
-    if images, ok := cache.Cache[key]; ok {
-        return images, nil
-    }
-
-    lbxFile, err := cache.LbxCache.GetLbxFile(lbxPath)
-    if err != nil {
-        return nil, err
-    }
-
-    sprites, err := lbxFile.ReadImages(index)
-    if err != nil {
-        return nil, err
-    }
-
-    var out []*ebiten.Image
-    for i := 0; i < len(sprites); i++ {
-        out = append(out, ebiten.NewImageFromImage(sprites[i]))
-    }
-
-    cache.Cache[key] = out
-
-    return out, nil
-}
-
-func (cache *ImageCache) GetImage(lbxFile string, spriteIndex int, animationIndex int) (*ebiten.Image, error) {
-    images, err := cache.GetImages(lbxFile, spriteIndex)
-    if err != nil {
-        return nil, err
-    }
-
-    if animationIndex < len(images) {
-        return images[animationIndex], nil
-    }
-
-    return nil, fmt.Errorf("invalid animation index: %d for %v:%v", animationIndex, lbxFile, spriteIndex)
-}
 
 type GameState int
 const (
