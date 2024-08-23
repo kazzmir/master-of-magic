074e0445dd7ab4f6e1ebecd7586568e00ac31652
Jon Rafkind <jon@rafkind.com>
2024-08-23 10:25:41 -0700

    use ui for build screen

--- game/magic/city/build-screen.go
+++ game/magic/city/build-screen.go
@@ -7,17 +7,31 @@ import (
     "github.com/kazzmir/master-of-magic/lib/font"
     "github.com/kazzmir/master-of-magic/game/magic/util"
     "github.com/kazzmir/master-of-magic/game/magic/units"
+    uilib "github.com/kazzmir/master-of-magic/game/magic/ui"
     "github.com/hajimehoshi/ebiten/v2"
 )
 
 type BuildScreen struct {
     LbxCache *lbx.LbxCache
-    ImageCache util.ImageCache
+    ImageCache *util.ImageCache
     City *City
-    TitleFont *font.Font
+    UI *uilib.UI
 }
 
 func MakeBuildScreen(cache *lbx.LbxCache, city *City) *BuildScreen {
+    imageCache := util.MakeImageCache(cache)
+
+    ui := makeBuildUI(cache, &imageCache, city)
+
+    return &BuildScreen{
+        LbxCache: cache,
+        ImageCache: &imageCache,
+        City: city,
+        UI: ui,
+    }
+}
+
+func makeBuildUI(cache *lbx.LbxCache, imageCache *util.ImageCache, city *City) *uilib.UI {
 
     fontLbx, err := cache.GetLbxFile("fonts.lbx")
     if err != nil {
@@ -33,52 +47,73 @@ func MakeBuildScreen(cache *lbx.LbxCache, city *City) *BuildScreen {
     
     titleFont := font.MakeOptimizedFont(fonts[2])
 
-    return &BuildScreen{
-        LbxCache: cache,
-        ImageCache: util.MakeImageCache(cache),
-        City: city,
-        TitleFont: titleFont,
+    var elements []*uilib.UIElement
+
+    buildingInfo, err := imageCache.GetImage("unitview.lbx", 31, 0)
+
+    if err == nil {
+        possibleBuildings := []Building{BuildingTradeGoods, BuildingHousing, BuildingBarracks, BuildingStables}
+        for i, building := range possibleBuildings {
+
+            element := &uilib.UIElement{
+                Draw: func(this *uilib.UIElement, screen *ebiten.Image) {
+                    var options ebiten.DrawImageOptions
+                    options.GeoM.Translate(0, 4 + float64(i * (buildingInfo.Bounds().Dy() + 1)))
+                    screen.DrawImage(buildingInfo, &options)
+                    x, y := options.GeoM.Apply(0, 0)
+                    titleFont.Print(screen, x + 2, y + 1, 1, building.String())
+                },
+            }
+
+            elements = append(elements, element)
+        }
     }
+
+    unitInfo, err := imageCache.GetImage("unitview.lbx", 32, 0)
+    if err == nil {
+        possibleUnits := []units.Unit{units.LizardSpearmen, units.LizardSettlers}
+        for i, unit := range possibleUnits {
+            element := &uilib.UIElement{
+                Draw: func(this *uilib.UIElement, screen *ebiten.Image) {
+                    var options ebiten.DrawImageOptions
+                    options.GeoM.Translate(240, 4 + float64(i * (unitInfo.Bounds().Dy() + 1)))
+                    screen.DrawImage(unitInfo, &options)
+                    x, y := options.GeoM.Apply(0, 0)
+                    titleFont.Print(screen, x + 2, y + 1, 1, unit.String())
+                },
+            }
+
+            elements = append(elements, element)
+        }
+    }
+
+    ui := &uilib.UI{
+        Draw: func(ui *uilib.UI, screen *ebiten.Image) {
+            mainInfo, err := imageCache.GetImage("unitview.lbx", 0, 0)
+            if err == nil {
+                var options ebiten.DrawImageOptions
+                options.GeoM.Translate(75, 0)
+                screen.DrawImage(mainInfo, &options)
+            }
+
+            ui.IterateElementsByLayer(func (element *uilib.UIElement){
+                if element.Draw != nil {
+                    element.Draw(element, screen)
+                }
+            })
+
+        },
+    }
+
+    ui.SetElementsFromArray(elements)
+
+    return ui
 }
 
 func (build *BuildScreen) Update() {
+    build.UI.StandardUpdate()
 }
 
 func (build *BuildScreen) Draw(screen *ebiten.Image) {
-    mainInfo, err := build.ImageCache.GetImage("unitview.lbx", 0, 0)
-    if err == nil {
-        var options ebiten.DrawImageOptions
-        options.GeoM.Translate(75, 0)
-        screen.DrawImage(mainInfo, &options)
-    }
-
-    buildingInfo, err := build.ImageCache.GetImage("unitview.lbx", 31, 0)
-    if err == nil {
-        possibleBuildings := []Building{BuildingTradeGoods, BuildingHousing, BuildingBarracks, BuildingStables}
-
-        var options ebiten.DrawImageOptions
-        options.GeoM.Translate(0, 4)
-        for _, building := range possibleBuildings {
-            screen.DrawImage(buildingInfo, &options)
-            x, y := options.GeoM.Apply(0, 0)
-            build.TitleFont.Print(screen, x + 2, y + 1, 1, building.String())
-
-            options.GeoM.Translate(0, float64(buildingInfo.Bounds().Dy() + 1))
-
-        }
-    }
-
-    unitInfo, err := build.ImageCache.GetImage("unitview.lbx", 32, 0)
-    if err == nil {
-        possibleUnits := []units.Unit{units.LizardSpearmen, units.LizardSettlers}
-
-        var options ebiten.DrawImageOptions
-        options.GeoM.Translate(240, 4)
-        for _, unit := range possibleUnits {
-            screen.DrawImage(unitInfo, &options)
-            x, y := options.GeoM.Apply(0, 0)
-            build.TitleFont.Print(screen, x + 2, y + 1, 1, unit.String())
-            options.GeoM.Translate(0, float64(unitInfo.Bounds().Dy() + 1))
-        }
-    }
+    build.UI.Draw(build.UI, screen)
 }
