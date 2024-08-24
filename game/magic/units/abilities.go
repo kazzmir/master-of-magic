b079b11c00bf2616acb71d16b2d1ab8f8e4f3643
Jon Rafkind <jon@rafkind.com>
2024-08-24 10:28:31 -0700

    add abilities list

---
+++ game/magic/units/abilities.go
@@ -0,0 +1,75 @@
+package units
+
+type Ability int
+
+const (
+    AbilityNone Ability = iota
+    // unit abilities
+    AbilityArmorPiercing
+    AbilityCauseFear
+    AbilityColdImmunity
+    AbilityConstruction
+    AbilityCreateOutpost
+    AbilityCreateUndead
+    AbilityDeathGaze
+    AbilityDeathImmunity
+    AbilityDispelEvil
+    AbilityDoomBoltSpell
+    AbilityDoomGaze
+    AbilityFireballSpell
+    AbilityFireBreath
+    AbilityFireImmunity
+    AbilityFirstStrike
+    AbilityForester
+    AbilityHealer
+    AbilityHealingSpell
+    AbilityHolyBonus
+    AbilityIllusion
+    AbilityIllusionsImmunity
+    AbilityInvisibility
+    AbilityLargeShield
+    AbilityLifeSteal
+    AbilityLightningBreath
+    AbilityLongRange
+    AbilityMagicImmunity
+    AbilityMeld
+    AbilityMerging
+    AbilityMissleImmunity
+    AbilityMountaineer
+    AbilityNegateFirstStrike
+    AbilityNonCorporeal
+    AbilityPathfinding
+    AbilityPlaneShift
+    AbilityPoisonImmunity
+    AbilityPoisonTouch
+    AbilityPurify
+    AbilityRegeneration
+    AbilityResistanceToAll
+    AbilityScouting
+    AbilityStoningGaze
+    AbilityStoningImmunity
+    AbilityStoningTouch
+    AbilitySummonDemons
+    AbilityTeleporting
+    AbilityThrown
+    AbilityWallCrusher
+    AbilityWeaponImmunity
+    AbilityWebSpell
+    AbilityWindWalking
+
+    // hero abilities
+    AbilityAgility
+    AbilityArcanePower
+    AbilityArmsmaster
+    AbilityBlademaster
+    AbilityCaster
+    AbilityCharmed
+    AbilityConstitution
+    AbilityLeadership
+    AbilityLegendary
+    AbilityLucky
+    AbilityMight
+    AbilityNoble
+    AbilityPrayermaster
+    AbilitySage
+)
