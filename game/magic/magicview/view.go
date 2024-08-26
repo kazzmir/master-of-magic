3af9a0342275ff5f151ed601993396598a8fa96c
Jon Rafkind <jon@rafkind.com>
2024-08-26 14:13:18 -0700

    distribute power across staffs

--- game/magic/magicview/view.go
+++ game/magic/magicview/view.go
@@ -1,6 +1,7 @@
 package magicview
 
 import (
+    // "log"
     "image"
 
     "github.com/kazzmir/master-of-magic/lib/lbx"
@@ -78,16 +79,64 @@ func (magic *MagicScreen) MakeUI() *uilib.UI {
 
     var elements []*uilib.UIElement
 
-    var manaPercent float64 = 0
+    var manaPercent float64 = 1.0 / 3
+    var researchPercent float64 = 1.0 / 3
+    var skillPercent float64 = 1.0 / 3
+
+    distribute := func(amount float64, update *float64, other1 *float64, other1Locked bool, other2 *float64, other2Locked bool){
+        if other1Locked && other2Locked {
+            return
+        }
+
+        diff := *update - amount
+        *update = amount
+
+        // distribute diff to the others
+        if !other1Locked && !other2Locked {
+            *other1 += diff / 2
+            *other2 += diff / 2
+
+            if *other1 < 0 {
+                *other2 += *other1
+                *other1 = 0
+            } else if *other2 < 0 {
+                *other1 += *other2
+                *other2 = 0
+            }
+
+        } else if !other1Locked {
+            *other1 += diff
+            if *other1 < 0 {
+                *update += *other1
+                *other1 = 0
+            }
+        } else if !other2Locked {
+            *other2 += diff
+
+            if *other2 < 0 {
+                *update += *other2
+                *other2= 0
+            }
+        }
+    }
 
     adjustManaPercent := func(amount float64){
-        manaPercent = amount
+        distribute(amount, &manaPercent, &researchPercent, magic.ResearchLocked, &skillPercent, magic.SkillLocked)
+        // log.Printf("mana: %v, research: %v, skill: %v total %v", manaPercent, researchPercent, skillPercent, manaPercent + researchPercent + skillPercent)
+    }
+
+    adjustResearchPercent := func(amount float64){
+        distribute(amount, &researchPercent, &manaPercent, magic.ManaLocked, &skillPercent, magic.SkillLocked)
+    }
+
+    adjustSkillPercent := func(amount float64){
+        distribute(amount, &skillPercent, &manaPercent, magic.ManaLocked, &researchPercent, magic.ResearchLocked)
     }
 
     manaLocked, err := magic.ImageCache.GetImage("magic.lbx", 15, 0)
     if err == nil {
         elements = append(elements, &uilib.UIElement{
-            Rect: image.Rect(27, 81, 27 + manaLocked.Bounds().Dx(), 81 + manaLocked.Bounds().Dy()),
+            Rect: image.Rect(27, 81, 27 + manaLocked.Bounds().Dx(), 81 + manaLocked.Bounds().Dy() - 2),
             LeftClick: func(element *uilib.UIElement){
                 magic.ManaLocked = !magic.ManaLocked
             },
@@ -96,16 +145,17 @@ func (magic *MagicScreen) MakeUI() *uilib.UI {
         manaStaff, _ := magic.ImageCache.GetImage("magic.lbx", 7, 0)
 
         posY := 0
-
-        powerStaff, _ := magic.ImageCache.GetImage("magic.lbx", 8, 0)
-        staffRect := image.Rect(33, 100, 38, 100 + powerStaff.Bounds().Dy())
+        manaPowerStaff, _ := magic.ImageCache.GetImage("magic.lbx", 8, 0)
+        staffRect := image.Rect(33, 102, 38, 102 + manaPowerStaff.Bounds().Dy())
 
         elements = append(elements, &uilib.UIElement{
             Rect: staffRect,
             LeftClick: func(element *uilib.UIElement){
-                // log.Printf("click mana staff at %v", manaStaff.Bounds().Dy() - posY)
-                amount := powerStaff.Bounds().Dy() - posY
-                adjustManaPercent(float64(amount) / float64(powerStaff.Bounds().Dy()))
+                if !magic.ManaLocked {
+                    // log.Printf("click mana staff at %v", manaStaff.Bounds().Dy() - posY)
+                    amount := manaPowerStaff.Bounds().Dy() - posY
+                    adjustManaPercent(float64(amount) / float64(manaPowerStaff.Bounds().Dy()))
+                }
             },
             Inside: func(element *uilib.UIElement, x, y int){
                 posY = y
@@ -116,8 +166,8 @@ func (magic *MagicScreen) MakeUI() *uilib.UI {
                 screen.DrawImage(manaStaff, &options)
 
                 if manaPercent > 0 {
-                    length := powerStaff.Bounds().Dy() - int(float64(powerStaff.Bounds().Dy()) * manaPercent)
-                    part := powerStaff.SubImage(image.Rect(0, length, powerStaff.Bounds().Dx(), powerStaff.Bounds().Dy())).(*ebiten.Image)
+                    length := manaPowerStaff.Bounds().Dy() - int(float64(manaPowerStaff.Bounds().Dy()) * manaPercent)
+                    part := manaPowerStaff.SubImage(image.Rect(0, length, manaPowerStaff.Bounds().Dx(), manaPowerStaff.Bounds().Dy())).(*ebiten.Image)
                     var options ebiten.DrawImageOptions
                     options.GeoM.Translate(32, float64(staffRect.Min.Y + length))
                     screen.DrawImage(part, &options)
@@ -134,20 +184,42 @@ func (magic *MagicScreen) MakeUI() *uilib.UI {
 
     researchLocked, err := magic.ImageCache.GetImage("magic.lbx", 16, 0)
     if err == nil {
+        researchStaff, _ := magic.ImageCache.GetImage("magic.lbx", 9, 0)
+
         elements = append(elements, &uilib.UIElement{
-            Rect: image.Rect(74, 81, 74 + researchLocked.Bounds().Dx(), 81 + researchLocked.Bounds().Dy()),
+            Rect: image.Rect(74, 81, 74 + researchLocked.Bounds().Dx(), 81 + researchLocked.Bounds().Dy() - 1),
             LeftClick: func(element *uilib.UIElement){
                 magic.ResearchLocked = !magic.ResearchLocked
             },
         })
 
+        researchPowerStaff, _ := magic.ImageCache.GetImage("magic.lbx", 10, 0)
+        posY := 0
+        staffRect := image.Rect(79, 102, 86, 102 + researchPowerStaff.Bounds().Dy())
+
         elements = append(elements, &uilib.UIElement{
+            Rect: staffRect,
+            LeftClick: func(element *uilib.UIElement){
+                if !magic.ResearchLocked {
+                    // log.Printf("click mana staff at %v", manaStaff.Bounds().Dy() - posY)
+                    amount := researchPowerStaff.Bounds().Dy() - posY
+                    adjustResearchPercent(float64(amount) / float64(researchPowerStaff.Bounds().Dy()))
+                }
+            },
+            Inside: func(element *uilib.UIElement, x, y int){
+                posY = y
+            },
             Draw: func(element *uilib.UIElement, screen *ebiten.Image) {
-                researchStaff, err := magic.ImageCache.GetImage("magic.lbx", 9, 0)
-                if err == nil {
+                var options ebiten.DrawImageOptions
+                options.GeoM.Translate(75, 85)
+                screen.DrawImage(researchStaff, &options)
+
+                if researchPercent > 0 {
+                    length := researchPowerStaff.Bounds().Dy() - int(float64(researchPowerStaff.Bounds().Dy()) * researchPercent)
+                    part := researchPowerStaff.SubImage(image.Rect(0, length, researchPowerStaff.Bounds().Dx(), researchPowerStaff.Bounds().Dy())).(*ebiten.Image)
                     var options ebiten.DrawImageOptions
-                    options.GeoM.Translate(75, 85)
-                    screen.DrawImage(researchStaff, &options)
+                    options.GeoM.Translate(float64(staffRect.Min.X), float64(staffRect.Min.Y + length))
+                    screen.DrawImage(part, &options)
                 }
 
                 if magic.ResearchLocked {
@@ -163,19 +235,42 @@ func (magic *MagicScreen) MakeUI() *uilib.UI {
     skillLocked, err := magic.ImageCache.GetImage("magic.lbx", 17, 0)
     if err == nil {
         elements = append(elements, &uilib.UIElement{
-            Rect: image.Rect(121, 81, 121 + skillLocked.Bounds().Dx(), 81 + skillLocked.Bounds().Dy()),
+            Rect: image.Rect(121, 81, 121 + skillLocked.Bounds().Dx(), 81 + skillLocked.Bounds().Dy() - 3),
             LeftClick: func(element *uilib.UIElement){
                 magic.SkillLocked = !magic.SkillLocked
             },
         })
 
+        skillPowerStaff, _ := magic.ImageCache.GetImage("magic.lbx", 10, 0)
+        posY := 0
+        staffRect := image.Rect(126, 102, 132, 102 + skillPowerStaff.Bounds().Dy())
+
         elements = append(elements, &uilib.UIElement{
+            Rect: staffRect,
+            LeftClick: func(element *uilib.UIElement){
+                if !magic.SkillLocked {
+                    // log.Printf("click mana staff at %v", manaStaff.Bounds().Dy() - posY)
+                    amount := skillPowerStaff.Bounds().Dy() - posY
+                    adjustSkillPercent(float64(amount) / float64(skillPowerStaff.Bounds().Dy()))
+                }
+            },
+            Inside: func(element *uilib.UIElement, x, y int){
+                posY = y
+            },
             Draw: func(element *uilib.UIElement, screen *ebiten.Image) {
                 skillStaff, err := magic.ImageCache.GetImage("magic.lbx", 11, 0)
                 if err == nil {
                     var options ebiten.DrawImageOptions
                     options.GeoM.Translate(122, 83)
                     screen.DrawImage(skillStaff, &options)
+                }
+
+                if skillPercent > 0 {
+                    length := skillPowerStaff.Bounds().Dy() - int(float64(skillPowerStaff.Bounds().Dy()) * skillPercent)
+                    part := skillPowerStaff.SubImage(image.Rect(0, length, skillPowerStaff.Bounds().Dx(), skillPowerStaff.Bounds().Dy())).(*ebiten.Image)
+                    var options ebiten.DrawImageOptions
+                    options.GeoM.Translate(float64(staffRect.Min.X), float64(staffRect.Min.Y + length))
+                    screen.DrawImage(part, &options)
                 }
 
                 if magic.SkillLocked {
