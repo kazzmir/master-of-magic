dafdf63633b1e76b581484da3aa32828e0e6a831
Jon Rafkind <jon@rafkind.com>
2024-08-26 08:49:02 -0700

    use ui for magic view

--- game/magic/magicview/view.go
+++ game/magic/magicview/view.go
@@ -2,9 +2,11 @@ package magicview
 
 import (
     "image"
+    // "log"
 
     "github.com/kazzmir/master-of-magic/lib/lbx"
     "github.com/kazzmir/master-of-magic/game/magic/util"
+    uilib "github.com/kazzmir/master-of-magic/game/magic/ui"
     "github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -18,6 +20,8 @@ const (
 type MagicScreen struct {
     Cache *lbx.LbxCache
     ImageCache util.ImageCache
+
+    UI *uilib.UI
 
     ManaLocked bool
     ResearchLocked bool
@@ -33,84 +37,120 @@ func MakeMagicScreen(cache *lbx.LbxCache) *MagicScreen {
         ResearchLocked: true,
         SkillLocked: true,
     }
+
+    magic.UI = magic.MakeUI()
+
     return magic
 }
 
+func (magic *MagicScreen) MakeUI() *uilib.UI {
+    ui := &uilib.UI{
+        Draw: func(ui *uilib.UI, screen *ebiten.Image) {
+            background, err := magic.ImageCache.GetImage("magic.lbx", 0, 0)
+            if err == nil {
+                var options ebiten.DrawImageOptions
+                screen.DrawImage(background, &options)
+            }
+
+            gemPositions := []image.Point{
+                image.Pt(24, 4),
+                image.Pt(101, 4),
+                image.Pt(178, 4),
+                image.Pt(255, 4),
+            }
+
+            for _, position := range gemPositions {
+                // FIXME: the gem color is based on what the banner color of the known wizard is
+                gemUnknown, err := magic.ImageCache.GetImage("magic.lbx", 6, 0)
+                if err == nil {
+                    var options ebiten.DrawImageOptions
+                    options.GeoM.Translate(float64(position.X), float64(position.Y))
+                    screen.DrawImage(gemUnknown, &options)
+                }
+            }
+
+            ui.IterateElementsByLayer(func (element *uilib.UIElement){
+                if element.Draw != nil {
+                    element.Draw(element, screen)
+                }
+            })
+        },
+    }
+
+    var elements []*uilib.UIElement
+
+    elements = append(elements, &uilib.UIElement{
+        Draw: func(element *uilib.UIElement, screen *ebiten.Image) {
+            manaStaff, err := magic.ImageCache.GetImage("magic.lbx", 7, 0)
+            if err == nil {
+                var options ebiten.DrawImageOptions
+                options.GeoM.Translate(29, 83)
+                screen.DrawImage(manaStaff, &options)
+            }
+
+            if magic.ManaLocked {
+                manaLocked, err := magic.ImageCache.GetImage("magic.lbx", 15, 0)
+                if err == nil {
+                    var options ebiten.DrawImageOptions
+                    options.GeoM.Translate(27, 81)
+                    screen.DrawImage(manaLocked, &options)
+                }
+            }
+        },
+    })
+
+    elements = append(elements, &uilib.UIElement{
+        Draw: func(element *uilib.UIElement, screen *ebiten.Image) {
+            researchStaff, err := magic.ImageCache.GetImage("magic.lbx", 9, 0)
+            if err == nil {
+                var options ebiten.DrawImageOptions
+                options.GeoM.Translate(75, 85)
+                screen.DrawImage(researchStaff, &options)
+            }
+
+            if magic.ResearchLocked {
+                researchLocked, err := magic.ImageCache.GetImage("magic.lbx", 16, 0)
+                if err == nil {
+                    var options ebiten.DrawImageOptions
+                    options.GeoM.Translate(74, 81)
+                    screen.DrawImage(researchLocked, &options)
+                }
+            }
+
+        },
+    })
+
+    elements = append(elements, &uilib.UIElement{
+        Draw: func(element *uilib.UIElement, screen *ebiten.Image) {
+            skillStaff, err := magic.ImageCache.GetImage("magic.lbx", 11, 0)
+            if err == nil {
+                var options ebiten.DrawImageOptions
+                options.GeoM.Translate(122, 83)
+                screen.DrawImage(skillStaff, &options)
+            }
+
+            if magic.SkillLocked {
+                skillLocked, err := magic.ImageCache.GetImage("magic.lbx", 17, 0)
+                if err == nil {
+                    var options ebiten.DrawImageOptions
+                    options.GeoM.Translate(121, 81)
+                    screen.DrawImage(skillLocked, &options)
+                }
+            }
+        },
+    })
+
+    ui.SetElementsFromArray(elements)
+
+    return ui
+}
+
 func (magic *MagicScreen) Update() MagicScreenState {
+    magic.UI.StandardUpdate()
 
     return MagicScreenStateRunning
 }
 
 func (magic *MagicScreen) Draw(screen *ebiten.Image){
-    background, err := magic.ImageCache.GetImage("magic.lbx", 0, 0)
-    if err == nil {
-        var options ebiten.DrawImageOptions
-        screen.DrawImage(background, &options)
-    }
-
-    gemPositions := []image.Point{
-        image.Pt(24, 4),
-        image.Pt(101, 4),
-        image.Pt(178, 4),
-        image.Pt(255, 4),
-    }
-
-    for _, position := range gemPositions {
-        // FIXME: the gem color is based on what the banner color of the known wizard is
-        gemUnknown, err := magic.ImageCache.GetImage("magic.lbx", 6, 0)
-        if err == nil {
-            var options ebiten.DrawImageOptions
-            options.GeoM.Translate(float64(position.X), float64(position.Y))
-            screen.DrawImage(gemUnknown, &options)
-        }
-    }
-
-    manaStaff, err := magic.ImageCache.GetImage("magic.lbx", 7, 0)
-    if err == nil {
-        var options ebiten.DrawImageOptions
-        options.GeoM.Translate(29, 83)
-        screen.DrawImage(manaStaff, &options)
-    }
-
-    if magic.ManaLocked {
-        manaLocked, err := magic.ImageCache.GetImage("magic.lbx", 15, 0)
-        if err == nil {
-            var options ebiten.DrawImageOptions
-            options.GeoM.Translate(27, 81)
-            screen.DrawImage(manaLocked, &options)
-        }
-    }
-
-    researchStaff, err := magic.ImageCache.GetImage("magic.lbx", 9, 0)
-    if err == nil {
-        var options ebiten.DrawImageOptions
-        options.GeoM.Translate(75, 85)
-        screen.DrawImage(researchStaff, &options)
-    }
-
-    if magic.ResearchLocked {
-        researchLocked, err := magic.ImageCache.GetImage("magic.lbx", 16, 0)
-        if err == nil {
-            var options ebiten.DrawImageOptions
-            options.GeoM.Translate(74, 81)
-            screen.DrawImage(researchLocked, &options)
-        }
-    }
-
-    skillStaff, err := magic.ImageCache.GetImage("magic.lbx", 11, 0)
-    if err == nil {
-        var options ebiten.DrawImageOptions
-        options.GeoM.Translate(122, 83)
-        screen.DrawImage(skillStaff, &options)
-    }
-
-    if magic.SkillLocked {
-        skillLocked, err := magic.ImageCache.GetImage("magic.lbx", 17, 0)
-        if err == nil {
-            var options ebiten.DrawImageOptions
-            options.GeoM.Translate(121, 81)
-            screen.DrawImage(skillLocked, &options)
-        }
-    }
-
+    magic.UI.Draw(magic.UI, screen)
 }
