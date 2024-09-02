d0ad9c50dc2f1435cf366344336886b8a4e1018d
Jon Rafkind <jon@rafkind.com>
2024-09-02 15:36:09 -0700

    start combat test

--- game/magic/combat/combat-screen.go
+++ game/magic/combat/combat-screen.go
@@ -1,30 +1,77 @@
 package combat
 
 import (
+    "math/rand"
+
+    "github.com/kazzmir/master-of-magic/lib/lbx"
     "github.com/kazzmir/master-of-magic/game/magic/units"
+    "github.com/kazzmir/master-of-magic/game/magic/util"
     "github.com/hajimehoshi/ebiten/v2"
 )
+
+type CombatState int
+
+const (
+    CombatStateRunning CombatState = iota
+    CombatStateDone
+)
+
+type Tile struct {
+    // index of grass/floor
+    Index int
+    // tree/rock on top, or -1 if nothing
+    ExtraObject int
+}
 
 type Army struct {
     Units []*units.Unit
 }
 
 type CombatScreen struct {
+    Cache *lbx.LbxCache
+    ImageCache util.ImageCache
     DefendingArmy *Army
     AttackingArmy *Army
+    Tiles [][]Tile
 }
 
-func NewCombatScreen(defendingArmy *Army, attackingArmy *Army) *CombatScreen {
+func makeTiles(width int, height int) [][]Tile {
+
+    maybeExtraTile := func() int {
+        if rand.Intn(10) == 0 {
+            return rand.Intn(10)
+        }
+        return -1
+    }
+
+    tiles := make([][]Tile, width)
+    for x := 0; x < len(tiles); x++ {
+        tiles[x] = make([]Tile, height)
+        for y := 0; y < len(tiles[x]); y++ {
+            tiles[x][y] = Tile{
+                Index: rand.Intn(48),
+                ExtraObject: maybeExtraTile(),
+            }
+        }
+
+    }
+
+    return tiles
+}
+
+func MakeCombatScreen(cache *lbx.LbxCache, defendingArmy *Army, attackingArmy *Army) *CombatScreen {
     return &CombatScreen{
+        Cache: cache,
+        ImageCache: util.MakeImageCache(cache),
         DefendingArmy: defendingArmy,
         AttackingArmy: attackingArmy,
+        Tiles: makeTiles(50, 50),
     }
 }
 
-func (combat *CombatScreen) Update() error {
-    return nil
+func (combat *CombatScreen) Update() CombatState {
+    return CombatStateRunning
 }
 
-func (combat *CombatScreen) Draw(screen *ebiten.Image) error {
-    return nil
+func (combat *CombatScreen) Draw(screen *ebiten.Image){
 }
---
+++ test/combat/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+    "log"
+    // "image/color"
+
+    "github.com/kazzmir/master-of-magic/lib/lbx"
+    "github.com/kazzmir/master-of-magic/game/magic/combat"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Engine struct {
+    LbxCache *lbx.LbxCache
+    CombatScreen *combat.CombatScreen
+}
+
+func NewEngine() (*Engine, error) {
+    cache := lbx.AutoCache()
+
+    defendingArmy := combat.Army{
+    }
+
+    attackingArmy := combat.Army{
+    }
+
+    return &Engine{
+        LbxCache: cache,
+        CombatScreen: combat.MakeCombatScreen(cache, &defendingArmy, &attackingArmy),
+    }, nil
+}
+
+func (engine *Engine) Update() error {
+
+    keys := make([]ebiten.Key, 0)
+    keys = inpututil.AppendJustPressedKeys(keys)
+
+    for _, key := range keys {
+        if key == ebiten.KeyEscape || key == ebiten.KeyCapsLock {
+            return ebiten.Termination
+        }
+    }
+
+    switch engine.CombatScreen.Update() {
+        case combat.CombatStateRunning:
+        case combat.CombatStateDone:
+            return ebiten.Termination
+    }
+
+    return nil
+}
+
+func (engine *Engine) Draw(screen *ebiten.Image) {
+    engine.CombatScreen.Draw(screen)
+}
+
+func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return data.ScreenWidth, data.ScreenHeight
+}
+
+func main(){
+    log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
+
+    ebiten.SetWindowSize(data.ScreenWidth * 5, data.ScreenHeight * 5)
+    ebiten.SetWindowTitle("combat screen")
+    ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+    engine, err := NewEngine()
+
+    if err != nil {
+        log.Printf("Error: unable to load engine: %v", err)
+        return
+    }
+
+    err = ebiten.RunGame(engine)
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
+}
