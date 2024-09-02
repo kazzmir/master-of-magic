e5e825191e04d3bd211767a0ed95b47317a6c0a0
Jon Rafkind <jon@rafkind.com>
2024-09-02 15:10:34 -0700

    lbx viewer can take any number of arguments

--- game/magic/combat/combat-screen.go
+++ game/magic/combat/combat-screen.go
@@ -2,6 +2,7 @@ package combat
 
 import (
     "github.com/kazzmir/master-of-magic/game/magic/units"
+    "github.com/hajimehoshi/ebiten/v2"
 )
 
 type Army struct {
@@ -11,4 +12,19 @@ type Army struct {
 type CombatScreen struct {
     DefendingArmy *Army
     AttackingArmy *Army
+}
+
+func NewCombatScreen(defendingArmy *Army, attackingArmy *Army) *CombatScreen {
+    return &CombatScreen{
+        DefendingArmy: defendingArmy,
+        AttackingArmy: attackingArmy,
+    }
+}
+
+func (combat *CombatScreen) Update() error {
+    return nil
+}
+
+func (combat *CombatScreen) Draw(screen *ebiten.Image) error {
+    return nil
 }
