95b9bcf129a9a8f883d41218c3897aae74c85bae
Jon Rafkind <jon@rafkind.com>
2024-09-08 13:40:40 -0700

    add benchmarks

--- game/magic/combat/path_test.go
+++ game/magic/combat/path_test.go
@@ -5,6 +5,7 @@ import (
     "strings"
     "image"
     "slices"
+    "math/rand"
 )
 
 func makeMap(data string) [][]int {
@@ -29,6 +30,38 @@ func makeMap(data string) [][]int {
     return out
 }
 
+func makeTileCost(tiles [][]int) func(int, int) float64 {
+    return func(x int, y int) float64 {
+        if x < 0 || y < 0 || y >= len(tiles) || x >= len(tiles[y]) {
+            return Infinity
+        }
+
+        return float64(tiles[y][x])
+    }
+}
+
+func makeNeighbors(tiles [][]int) func(int, int) []image.Point {
+    return func(cx int, cy int) []image.Point {
+        var out []image.Point
+
+        for x := -1; x <= 1; x++ {
+            for y := -1; y <= 1; y++ {
+                dx := cx + x
+                dy := cy + y
+                if dx == cx && dy == cy {
+                    continue
+                }
+
+                if dx >= 0 && dy >= 0 && dy < len(tiles) && dx < len(tiles[dy]) {
+                    out = append(out, image.Pt(dx, dy))
+                }
+            }
+        }
+
+        return out
+    }
+}
+
 func TestPath1(test *testing.T){
 
     tiles := makeMap(`
@@ -49,33 +82,8 @@ func TestPath1(test *testing.T){
         image.Pt(3, 3),
     }
 
-    tileCost := func(x int, y int) float64 {
-        if x < 0 || y < 0 || y >= len(tiles) || x >= len(tiles[y]) {
-            return Infinity
-        }
-
-        return float64(tiles[y][x])
-    }
-
-    neighbors := func(cx int, cy int) []image.Point {
-        var out []image.Point
-
-        for x := -1; x <= 1; x++ {
-            for y := -1; y <= 1; y++ {
-                dx := cx + x
-                dy := cy + y
-                if dx == cx && dy == cy {
-                    continue
-                }
-
-                if dx >= 0 && dy >= 0 && dy < len(tiles) && dx < len(tiles[dy]) {
-                    out = append(out, image.Pt(dx, dy))
-                }
-            }
-        }
-
-        return out
-    }
+    tileCost := makeTileCost(tiles)
+    neighbors := makeNeighbors(tiles)
 
     path, ok := FindPath(start, end, 10, tileCost, neighbors)
     if !ok {
@@ -93,5 +101,59 @@ func TestPath1(test *testing.T){
     path2, ok := FindPath(start, end, 3, tileCost, neighbors)
     if ok {
         test.Errorf("expected no path, but found one: %v", path2)
+    }
+}
+
+func makeRandomMap(rows int, columns int, value int) [][]int {
+    var out [][]int
+
+    for y := 0; y < rows; y++ {
+        var row []int
+        for x := 0; x < columns; x++ {
+            row = append(row, rand.Intn(value))
+        }
+        out = append(out, row)
+    }
+
+    return out
+}
+
+func TestStress(test *testing.T){
+    tiles := makeRandomMap(100, 100, 10)
+
+    start := image.Pt(0, 0)
+    end := image.Pt(99, 99)
+
+    _, ok := FindPath(start, end, 100000, makeTileCost(tiles), makeNeighbors(tiles))
+    if !ok {
+        test.Errorf("unable to find path")
+    }
+
+    for i := 0; i < 10; i++ {
+        start = image.Pt(rand.Intn(100), rand.Intn(100))
+        end = image.Pt(rand.Intn(100), rand.Intn(100))
+        if start.Eq(end) {
+            continue
+        }
+        _, ok := FindPath(start, end, 100000, makeTileCost(tiles), makeNeighbors(tiles))
+        if !ok {
+            test.Errorf("unable to find path")
+        }
+    }
+}
+
+func BenchmarkLarge(bench *testing.B){
+    tiles := makeRandomMap(100, 100, 10)
+    bench.ResetTimer()
+    for range bench.N {
+        FindPath(image.Pt(0, 0), image.Pt(99, 99), 100000, makeTileCost(tiles), makeNeighbors(tiles))
+    }
+}
+
+func BenchmarkSmall(bench *testing.B){
+    tiles := makeRandomMap(20, 20, 10)
+    bench.ResetTimer()
+    for range bench.N {
+        FindPath(image.Pt(0, 0), image.Pt(19, 19), 100000, makeTileCost(tiles), makeNeighbors(tiles))
     }
 }
