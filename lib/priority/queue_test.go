34491b4c9d65bd1c7a6d14701127d83aede7b709
Jon Rafkind <jon@rafkind.com>
2024-09-08 09:40:21 -0700

    test randomly creating heap

--- lib/priority/queue_test.go
+++ lib/priority/queue_test.go
@@ -2,6 +2,7 @@ package priority
 
 import (
     "testing"
+    "math/rand"
 )
 
 func TestBasic(test *testing.T){
@@ -24,6 +25,33 @@ func TestBasic(test *testing.T){
     top = heap.RemoveMin()
     if top != 3 {
         test.Errorf("Expected min 3, got %d", top)
+    }
+
+    if !heap.IsEmpty() {
+        test.Errorf("Expected empty heap")
+    }
+}
+
+func TestInsert(test *testing.T){
+    heap := MakeEmptyHeap()
+
+    N := 1000
+
+    for i := 0; i < N; i++ {
+        heap.Insert(rand.Intn(10000))
+    }
+
+    if heap.Size() != N {
+        test.Errorf("Expected size %v, got %d", N, heap.Size())
+    }
+
+    last := -1
+    for i := 0; i < N; i++ {
+        top := heap.RemoveMin()
+        if top < last {
+            test.Errorf("Expected increasing order, got %d after %d", top, last)
+        }
+        last = top
     }
 
     if !heap.IsEmpty() {
