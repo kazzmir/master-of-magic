0fb7dfac8cfb0ab94798077566ca0f223a9f17ab
Jon Rafkind <jon@rafkind.com>
2024-10-05 14:11:11 -0700

    show animations, reveal fog

--- game/magic/game/cast.go
+++ game/magic/game/cast.go
@@ -1,10 +1,15 @@
 package game
 
 import (
+    _ "log"
+    "image"
+    "math/rand"
+
     "github.com/kazzmir/master-of-magic/lib/coroutine"
     playerlib "github.com/kazzmir/master-of-magic/game/magic/player"
     "github.com/kazzmir/master-of-magic/game/magic/spellbook"
     "github.com/kazzmir/master-of-magic/game/magic/util"
+    // "github.com/kazzmir/master-of-magic/game/magic/data"
 
     "github.com/hajimehoshi/ebiten/v2"
 )
@@ -12,35 +17,73 @@ import (
 func (game *Game) doCastSpell(yield coroutine.YieldFunc, player *playerlib.Player, spell spellbook.Spell) {
     switch spell.Name {
         case "Earth Lore":
-            game.doCastEarthLore(yield, player)
+            screenX := 100
+            screenY := 100
+            game.doCastEarthLore(yield, player, screenX, screenY)
+
+            tileX := game.cameraX + screenX / game.Map.TileWidth()
+            tileY := game.cameraY + screenY / game.Map.TileHeight()
+
+            player.LiftFog(tileX, tileY, 5)
     }
 }
 
-func (game *Game) doCastEarthLore(yield coroutine.YieldFunc, player *playerlib.Player) {
+func (game *Game) doCastEarthLore(yield coroutine.YieldFunc, player *playerlib.Player, x int, y int) {
     oldDrawer := game.Drawer
     defer func(){
         game.Drawer = oldDrawer
     }()
 
     pics, _ := game.ImageCache.GetImages("specfx.lbx", 0)
-    animation := util.MakeAnimation(pics, false)
-    animationAlive := true
+
+    type SpellAnimation struct {
+        Delay uint64
+        Animation *util.Animation
+        Done bool
+        Point image.Point
+    }
+
+    var animations []SpellAnimation
+
+    for i := 0; i < 5; i++ {
+        animation := util.MakeAnimation(pics, false)
+        animations = append(animations, SpellAnimation{
+            Delay: uint64(rand.Intn(20)),
+            Animation: animation,
+            Done: false,
+            Point: image.Point{X: x + rand.Intn(100) - 50, Y: y + rand.Intn(100) - 50},
+        })
+    }
 
     game.Drawer = func(screen *ebiten.Image, game *Game) {
         oldDrawer(screen, game)
 
-        if animationAlive {
-            var options ebiten.DrawImageOptions
-            options.GeoM.Translate(20, 20)
-            screen.DrawImage(animation.Frame(), &options)
+        for i := 0; i < len(animations); i++ {
+            if animations[i].Delay == 0 && animations[i].Done == false {
+                var options ebiten.DrawImageOptions
+                options.GeoM.Translate(float64(animations[i].Point.X), float64(animations[i].Point.Y))
+                screen.DrawImage(animations[i].Animation.Frame(), &options)
+            }
         }
     }
 
-    for i := 0; i < 100; i++ {
+    // FIXME: play earth lore sound
+
+    quit := false
+    for !quit {
         game.Counter += 1
 
-        if game.Counter % 5 == 0 && animationAlive {
-            animationAlive = animation.Next()
+        quit = true
+        for j := 0; j < len(animations); j++ {
+            if animations[j].Delay > 0 {
+                animations[j].Delay -= 1
+            } else if game.Counter % 5 == 0 {
+                animations[j].Done = !animations[j].Animation.Next()
+            }
+
+            if animations[j].Delay > 0 || !animations[j].Done {
+                quit = false
+            }
         }
 
         yield()
