04cbb5d5140bd5e6040c4f43cada1ca21063d275
Jon Rafkind <jon@rafkind.com>
2024-10-05 14:26:37 -0700

    use right animation and sound

--- game/magic/game/cast.go
+++ game/magic/game/cast.go
@@ -2,13 +2,12 @@ package game
 
 import (
     _ "log"
-    "image"
-    "math/rand"
 
     "github.com/kazzmir/master-of-magic/lib/coroutine"
     playerlib "github.com/kazzmir/master-of-magic/game/magic/player"
     "github.com/kazzmir/master-of-magic/game/magic/spellbook"
     "github.com/kazzmir/master-of-magic/game/magic/util"
+    "github.com/kazzmir/master-of-magic/game/magic/audio"
     // "github.com/kazzmir/master-of-magic/game/magic/data"
 
     "github.com/hajimehoshi/ebiten/v2"
@@ -34,56 +33,34 @@ func (game *Game) doCastEarthLore(yield coroutine.YieldFunc, player *playerlib.P
         game.Drawer = oldDrawer
     }()
 
-    pics, _ := game.ImageCache.GetImages("specfx.lbx", 0)
+    pics, _ := game.ImageCache.GetImages("specfx.lbx", 45)
 
-    type SpellAnimation struct {
-        Delay uint64
-        Animation *util.Animation
-        Done bool
-        Point image.Point
-    }
-
-    var animations []SpellAnimation
-
-    for i := 0; i < 5; i++ {
-        animation := util.MakeAnimation(pics, false)
-        animations = append(animations, SpellAnimation{
-            Delay: uint64(rand.Intn(20)),
-            Animation: animation,
-            Done: false,
-            Point: image.Point{X: x + rand.Intn(100) - 50, Y: y + rand.Intn(100) - 50},
-        })
-    }
+    animation := util.MakeAnimation(pics, false)
 
     game.Drawer = func(screen *ebiten.Image, game *Game) {
         oldDrawer(screen, game)
 
-        for i := 0; i < len(animations); i++ {
-            if animations[i].Delay == 0 && animations[i].Done == false {
-                var options ebiten.DrawImageOptions
-                options.GeoM.Translate(float64(animations[i].Point.X), float64(animations[i].Point.Y))
-                screen.DrawImage(animations[i].Animation.Frame(), &options)
-            }
-        }
+        var options ebiten.DrawImageOptions
+        options.GeoM.Translate(float64(x - animation.Frame().Bounds().Dx() / 2), float64(y - animation.Frame().Bounds().Dy() / 2))
+        screen.DrawImage(animation.Frame(), &options)
     }
 
     // FIXME: play earth lore sound
+    // probably soundfx.lbx sound 28
+    // newsound.fx 18
+
+    sound, err := audio.LoadNewSound(game.Cache, 18)
+    if err == nil {
+        sound.Play()
+    }
 
     quit := false
     for !quit {
         game.Counter += 1
 
-        quit = true
-        for j := 0; j < len(animations); j++ {
-            if animations[j].Delay > 0 {
-                animations[j].Delay -= 1
-            } else if game.Counter % 5 == 0 {
-                animations[j].Done = !animations[j].Animation.Next()
-            }
-
-            if animations[j].Delay > 0 || !animations[j].Done {
-                quit = false
-            }
+        quit = false
+        if game.Counter % 6 == 0 {
+            quit = !animation.Next()
         }
 
         yield()
