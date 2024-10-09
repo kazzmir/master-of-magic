96d283bcce84333cd296f608c6550776fcb43ac5
Jon Rafkind <jon@rafkind.com>
2024-10-09 15:15:32 -0700

    fix hero levels too

--- game/magic/units/experience.go
+++ game/magic/units/experience.go
@@ -87,17 +87,70 @@ func GetNormalExperienceLevel(experience int, warlordRetort bool, crusade bool) 
     return ExperienceElite
 }
 
-func (level HeroExperienceLevel) ExperienceRequired() int {
+func (level HeroExperienceLevel) ExperienceRequired(warlordRetort bool, crusade bool) int {
     switch level {
         case ExperienceHero: return 0
-        case ExperienceMyrmidon: return 20
-        case ExperienceCaptain: return 60
-        case ExperienceCommander: return 120
-        case ExperienceChampionHero: return 200
-        case ExperienceLord: return 300
-        case ExperienceGrandLord: return 450
-        case ExperienceSuperHero: return 600
-        case ExperienceDemiGod: return 1000
+        case ExperienceMyrmidon:
+            if warlordRetort || crusade {
+                return 0
+            }
+            return 20
+        case ExperienceCaptain:
+            if warlordRetort && crusade {
+                return 0
+            }
+            if warlordRetort || crusade {
+                return 20
+            }
+            return 60
+        case ExperienceCommander:
+            if warlordRetort && crusade {
+                return 20
+            }
+            if warlordRetort || crusade {
+                return 60
+            }
+            return 120
+        case ExperienceChampionHero:
+            if warlordRetort && crusade {
+                return 60
+            }
+            if warlordRetort || crusade {
+                return 120
+            }
+            return 200
+        case ExperienceLord:
+            if warlordRetort && crusade {
+                return 120
+            }
+            if warlordRetort || crusade {
+                return 200
+            }
+            return 300
+        case ExperienceGrandLord:
+            if warlordRetort && crusade {
+                return 200
+            }
+            if warlordRetort || crusade {
+                return 300
+            }
+            return 450
+        case ExperienceSuperHero:
+            if warlordRetort && crusade {
+                return 300
+            }
+            if warlordRetort || crusade {
+                return 450
+            }
+            return 600
+        case ExperienceDemiGod:
+            if warlordRetort && crusade {
+                return 450
+            }
+            if warlordRetort || crusade {
+                return 600
+            }
+            return 1000
     }
 
     return 0
@@ -111,7 +164,7 @@ func GetHeroExperienceLevel(experience int, warlordRetort bool, crusade bool) He
     }
 
     for i := 0; i < len(levels) - 1; i++ {
-        if experience < levels[i + 1].ExperienceRequired() {
+        if experience < levels[i + 1].ExperienceRequired(warlordRetort, crusade) {
             return levels[i]
         }
     }
