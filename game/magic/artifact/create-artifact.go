84a7d6cb83828201530c56ae09668bc29036d5d3
Jon Rafkind <jon@rafkind.com>
2024-10-10 10:37:39 -0700

    show selectable powers

--- game/magic/artifact/create-artifact.go
+++ game/magic/artifact/create-artifact.go
@@ -1,11 +1,16 @@
 package artifact
 
 import (
+    "fmt"
+    "image"
+    "log"
+
     uilib "github.com/kazzmir/master-of-magic/game/magic/ui"
     "github.com/kazzmir/master-of-magic/game/magic/spellbook"
     "github.com/kazzmir/master-of-magic/game/magic/util"
     "github.com/kazzmir/master-of-magic/lib/coroutine"
     "github.com/kazzmir/master-of-magic/lib/lbx"
+    "github.com/kazzmir/master-of-magic/lib/font"
 
     "github.com/hajimehoshi/ebiten/v2"
     // "github.com/hajimehoshi/ebiten/v2/inpututil"
@@ -27,39 +32,72 @@ const (
 )
 
 type Power interface {
+    String() string
 }
 
 type PowerAttack struct {
     Amount int
 }
 
+func (p *PowerAttack) String() string {
+    return fmt.Sprintf("+%v Attack", p.Amount)
+}
+
 type PowerDefense struct {
     Amount int
+}
+
+func (p *PowerDefense) String() string {
+    return fmt.Sprintf("+%v Defense", p.Amount)
 }
 
 type PowerToHit struct {
     Amount int
 }
 
+func (p *PowerToHit) String() string {
+    return fmt.Sprintf("+%v To Hit", p.Amount)
+}
+
 type PowerSpellSkill struct {
     Amount int
+}
+
+func (p *PowerSpellSkill) String() string {
+    return fmt.Sprintf("+%v Spell Skill", p.Amount)
 }
 
 type PowerSpellSave struct {
     Amount int
 }
 
+func (p *PowerSpellSave) String() string {
+    return fmt.Sprintf("%v Spell Save", p.Amount)
+}
+
 type PowerResistance struct {
     Amount int
+}
+
+func (p *PowerResistance) String() string {
+    return fmt.Sprintf("+%v Resistance", p.Amount)
 }
 
 type PowerMovement struct {
     Amount int
 }
 
+func (p *PowerMovement) String() string {
+    return fmt.Sprintf("+%v Movement", p.Amount)
+}
+
 type PowerSpellCharges struct {
     Spell spellbook.Spell
     Charges int
+}
+
+func (p *PowerSpellCharges) String() string {
+    return "Spell Charges"
 }
 
 type Artifact struct {
@@ -69,11 +107,61 @@ type Artifact struct {
     Powers []Power
 }
 
+func makePowers(imageCache *util.ImageCache, powerFont *font.Font, powers []Power) []*uilib.UIElement {
+    var elements []*uilib.UIElement
+    x := 7
+    y := 40
+    selectCount := 0
+    for _, power := range powers {
+        rect := image.Rect(x, y, x + int(powerFont.MeasureTextWidth(power.String(), 1)), y + powerFont.Height())
+        selected := false
+        elements = append(elements, &uilib.UIElement{
+            Rect: rect,
+            LeftClick: func(element *uilib.UIElement){
+                if selected {
+                    selectCount -= 1
+                    selected = false
+                } else {
+                    // FIXME: max of 4
+                    selectCount += 1
+                    selected = true
+                }
+            },
+            Draw: func(element *uilib.UIElement, screen *ebiten.Image){
+                // draw in bright yellow if selected
+                powerFont.Print(screen, float64(rect.Min.X), float64(rect.Min.Y), 1, ebiten.ColorScale{}, power.String())
+            },
+        })
+
+        y += powerFont.Height()
+    }
+
+    return elements
+}
+
+func makePowerFont(cache *lbx.LbxCache) *font.Font {
+    fontLbx, err := cache.GetLbxFile("fonts.lbx")
+    if err != nil {
+        log.Printf("Unable to read fonts.lbx: %v", err)
+        return nil
+    }
+
+    fonts, err := font.ReadFonts(fontLbx, 0)
+    if err != nil {
+        log.Printf("Unable to read fonts from fonts.lbx: %v", err)
+        return nil
+    }
+
+    return font.MakeOptimizedFont(fonts[3])
+}
+
 /* returns the artifact that was created and true,
  * otherwise false for cancelled
  */
 func ShowCreateArtifactScreen(yield coroutine.YieldFunc, cache *lbx.LbxCache, draw *func(*ebiten.Image)) (*Artifact, bool) {
     quit := false
+
+    powerFont := makePowerFont(cache)
 
     imageCache := util.MakeImageCache(cache)
 
@@ -93,9 +181,58 @@ func ShowCreateArtifactScreen(yield coroutine.YieldFunc, cache *lbx.LbxCache, dr
 
     ui.SetElementsFromArray(nil)
 
+    type ItemIndex int
+    const (
+        ItemSword ItemIndex = iota
+        ItemMace
+        ItemAxe
+        ItemBow
+        ItemStaff
+        ItemWand
+        ItemMisc
+        ItemShield
+        ItemChain
+        ItemPlate
+    )
+
+    // ui elements for powers that can be selected, based on what item is selected
+    powers := make(map[ItemIndex][]*uilib.UIElement)
+
+    powers[ItemSword] = makePowers(&imageCache, powerFont, []Power{
+        &PowerAttack{Amount: 1},
+        &PowerAttack{Amount: 2},
+        &PowerAttack{Amount: 3},
+        &PowerDefense{Amount: 1},
+        &PowerDefense{Amount: 2},
+        &PowerDefense{Amount: 3},
+        &PowerToHit{Amount: 1},
+        &PowerToHit{Amount: 2},
+        &PowerToHit{Amount: 3},
+        &PowerSpellSkill{Amount: 5},
+        &PowerSpellSkill{Amount: 10},
+    })
+
+    powers[ItemWand] = makePowers(&imageCache, powerFont, []Power{
+        &PowerAttack{Amount: 1},
+        &PowerAttack{Amount: 2},
+        &PowerToHit{Amount: 1},
+        &PowerSpellSkill{Amount: 5},
+        &PowerSpellSkill{Amount: 10},
+        &PowerSpellSave{Amount: -1},
+        &PowerSpellSave{Amount: -2},
+    })
+
+    updatePowers := func(index ItemIndex){
+        for _, elements := range powers {
+            ui.RemoveElements(elements)
+        }
+
+        ui.AddElements(powers[index])
+    }
+
     var selectedButton *uilib.UIElement
 
-    makeButton := func(x int, y int, unselected int, selected int) *uilib.UIElement {
+    makeButton := func(x int, y int, unselected int, selected int, item ItemIndex) *uilib.UIElement {
         index := 0
         imageRect, _ := imageCache.GetImage("spellscr.lbx", unselected, 0)
         rect := util.ImageRect(x, y, imageRect)
@@ -107,6 +244,8 @@ func ShowCreateArtifactScreen(yield coroutine.YieldFunc, cache *lbx.LbxCache, dr
             LeftClickRelease: func(element *uilib.UIElement){
                 selectedButton = element
                 index = 0
+
+                updatePowers(item)
             },
             Draw: func(element *uilib.UIElement, screen *ebiten.Image){
                 var options ebiten.DrawImageOptions
@@ -131,7 +270,7 @@ func ShowCreateArtifactScreen(yield coroutine.YieldFunc, cache *lbx.LbxCache, dr
         x := 156 + (i % 5) * (tmpImage.Bounds().Dx() + 2)
         y := 3 + (i / 5) * (tmpImage.Bounds().Dy() + 2)
 
-        ui.AddElement(makeButton(x, y, unselectedImageStart + i, selectedImageStart + i))
+        ui.AddElement(makeButton(x, y, unselectedImageStart + i, selectedImageStart + i, ItemIndex(i)))
     }
 
     *draw = func(screen *ebiten.Image) {
