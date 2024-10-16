4420a353dd2ec78e31876b8ac8db11baa79b316b
Jon Rafkind <jon@rafkind.com>
2024-10-16 09:10:46 -0700

    add all global enchantments

--- game/magic/data/enchantment.go
+++ game/magic/data/enchantment.go
@@ -4,6 +4,28 @@ type Enchantment int
 
 const (
     EnchantmentNone Enchantment = iota
+    EnchantmentAwareness
+    EnchantmentDetectMagic
+    EnchantmentCharmOfLife
     EnchantmentCrusade
+    EnchantmentHolyArms
+    EnchantmentJustCause
+    EnchantmentLifeForce
+    EnchantmentPlanarSeal
+    EnchantmentTranquility
+    EnchantmentHerbMastery
+    EnchantmentNatureAwareness
+    EnchantmentNaturesWrath
+    EnchantmentAuraOfMajesty
+    EnchantmentSuppressMagic
+    EnchantmentTimeStop
+    EnchantmentWindMastery
+    EnchantmentArmageddon
+    EnchantmentChaosSurge
+    EnchantmentDoomMastery
+    EnchantmentGreatWasting
+    EnchantmentMeteorStorm
+    EnchantmentEternalNight
+    EnchantmentEvilOmens
+    EnchantmentZombieMastery
 )
-
