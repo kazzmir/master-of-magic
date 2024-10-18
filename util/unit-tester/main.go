d9b9cf6187f7b6d974104fcb2f12991e94bb26af
Jon Rafkind <jon@rafkind.com>
2024-10-17 21:29:36 -0700

    add utility to display units with missing fields, update lizardmen units (#102)
    * start helper program to ensure units have proper values
    
    * some lizardmen units
    
    * add rest of lizard units

---
+++ util/unit-tester/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "os"
+
+    "github.com/kazzmir/master-of-magic/game/magic/units"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+)
+
+func main(){
+    specifyRace := ""
+    if len(os.Args) > 1 {
+        specifyRace = os.Args[1]
+    }
+
+    total := 0
+    missingTotal := 0
+    for _, unit := range units.AllUnits {
+        if specifyRace != "" && strings.ToLower(specifyRace) != strings.ToLower(unit.Race.String()) {
+            continue
+        }
+
+        total += 1
+        var missing []string
+
+        if unit.CombatLbxFile == "" {
+            missing = append(missing, "CombatLbxFile")
+        }
+
+        if unit.MovementSpeed == 0 {
+            missing = append(missing, "MovementSpeed")
+        }
+
+        if unit.Defense == 0 {
+            missing = append(missing, "Defense")
+        }
+
+        if unit.HitPoints == 0 {
+            missing = append(missing, "HitPoints")
+        }
+
+        if unit.Count == 0 {
+            missing = append(missing, "Count")
+        }
+
+        if unit.Resistance == 0 {
+            missing = append(missing, "Resistance")
+        }
+
+        if unit.Name == "" {
+            missing = append(missing, "Name")
+        }
+
+        if unit.Name != "Settlers" && unit.MeleeAttackPower == 0 && unit.RangedAttackPower == 0 {
+            missing = append(missing, "MeleeAttackPower or RangedAttackPower")
+        }
+
+        if unit.Race == data.RaceNone {
+            missing = append(missing, "Race")
+        }
+
+        if unit.Race != data.RaceHero && unit.Race != data.RaceFantastic && unit.ProductionCost == 0 {
+            missing = append(missing, "ProductionCost")
+        }
+
+        if len(missing) > 0 {
+            fmt.Printf("Unit %s %s is missing the following fields: %v\n", unit.Name, unit.Race, strings.Join(missing, ", "))
+            missingTotal += 1
+        }
+    }
+
+    fmt.Printf("Total: %v Missing fields: %v\n", total, missingTotal)
+
+}
