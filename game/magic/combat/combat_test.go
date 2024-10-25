461030f996cf3248d9168508e59c0fe1656105d9
Jon Rafkind <jon@rafkind.com>
2024-10-24 23:59:17 -0700

    add simple benchmark for angle test

--- game/magic/combat/combat_test.go
+++ game/magic/combat/combat_test.go
@@ -3,6 +3,8 @@ package combat
 import (
     "testing"
     "math"
+
+    "github.com/kazzmir/master-of-magic/game/magic/units"
 )
 
 func TestAngle(test *testing.T){
@@ -27,4 +29,24 @@ func TestAngle(test *testing.T){
         test.Errorf("Error check 0 not in pi spread pi/3")
     }
 
+}
+
+func tmp(f units.Facing){
+    _ = f
+}
+
+func BenchmarkAngle(bench *testing.B){
+    var final units.Facing
+    for range bench.N {
+        // for angle := range 360 {
+        angle := 32
+            radians := float64(angle) * math.Pi / 180
+            facing := computeFacing(radians)
+            if facing == units.FacingDown {
+                final = facing
+            }
+        // }
+    }
+
+    tmp(final)
 }
