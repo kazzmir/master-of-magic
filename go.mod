68733769fdd1b42c0b7ff339dd6f5d845c86f72a
Jon Rafkind <jon@rafkind.com>
2024-11-26 23:37:25 -0800

    make two simple labels in ui

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kazzmir/master-of-magic
 go 1.22.2
 
 require (
+	github.com/ebitenui/ebitenui v0.6.0
 	github.com/hajimehoshi/ebiten/v2 v2.8.5
 	gitlab.com/gomidi/midi/v2 v2.2.19
 )
@@ -14,6 +15,7 @@ require (
 	github.com/ebitengine/purego v0.8.1 // indirect
 	github.com/go-text/typesetting v0.2.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
 	golang.org/x/image v0.22.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
--- util/combat-simulator/main.go
+++ util/combat-simulator/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
     "log"
+    "image/color"
+
     "github.com/kazzmir/master-of-magic/lib/lbx"
     "github.com/kazzmir/master-of-magic/game/magic/data"
+    "github.com/kazzmir/master-of-magic/util/common"
+
     "github.com/hajimehoshi/ebiten/v2"
     "github.com/hajimehoshi/ebiten/v2/inpututil"
+    "github.com/hajimehoshi/ebiten/v2/text/v2"
+
+    "github.com/ebitenui/ebitenui"
+    "github.com/ebitenui/ebitenui/widget"
 )
 
 const EngineWidth = 800
@@ -20,12 +28,17 @@ const (
 type Engine struct {
     Cache *lbx.LbxCache
     Mode EngineMode
+    UI *ebitenui.UI
 }
 
-func MakeEngine(cache *lbx.LbxCache) Engine {
-    return Engine{
+func MakeEngine(cache *lbx.LbxCache) *Engine {
+    engine := Engine{
         Cache: cache,
     }
+
+    engine.UI = engine.MakeUI()
+
+    return &engine
 }
 
 func (engine *Engine) Update() error {
@@ -38,10 +51,13 @@ func (engine *Engine) Update() error {
         }
     }
 
+    engine.UI.Update()
+
     return nil
 }
 
 func (engine *Engine) Draw(screen *ebiten.Image) {
+    engine.UI.Draw(screen)
 }
 
 func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -55,13 +71,46 @@ func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, scre
     return 0, 0
 }
 
+func (engine *Engine) MakeUI() *ebitenui.UI {
+    face, _ := loadFont(18)
+
+    rootContainer := widget.NewContainer(widget.ContainerOpts.Layout(widget.NewRowLayout(widget.RowLayoutOpts.Direction(widget.DirectionVertical))))
+
+    label1 := widget.NewText(widget.TextOpts.Text("Hello!", face, color.White))
+
+    rootContainer.AddChild(label1)
+
+    label2 := widget.NewText(widget.TextOpts.Text("Everyone!", face, color.White))
+    rootContainer.AddChild(label2)
+
+    ui := ebitenui.UI{
+        Container: rootContainer,
+    }
+
+    return &ui
+}
+
+func loadFont(size float64) (text.Face, error) {
+    source, err := common.LoadFont()
+
+    if err != nil {
+        log.Fatal(err)
+        return nil, err
+    }
+
+    return &text.GoTextFace{
+        Source: source,
+        Size:   size,
+    }, nil
+}
+
 func main(){
     cache := lbx.AutoCache()
 
     engine := MakeEngine(cache)
     ebiten.SetWindowSize(800, 600)
 
-    err := ebiten.RunGame(&engine)
+    err := ebiten.RunGame(engine)
     if err != nil {
         log.Printf("Error: %v", err)
     }
