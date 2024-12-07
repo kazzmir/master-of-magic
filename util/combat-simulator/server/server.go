3cba295e9f951a49444fb9e46346866a9a6e4d79
Jon Rafkind <jon@rafkind.com>
2024-12-06 22:20:05 -0800

    start report server

--- util/combat-simulator/server/server.go
+++ util/combat-simulator/server/server.go
@@ -2,10 +2,39 @@ package main
 
 import (
     "log"
+    "net/http"
+    "fmt"
+    "crypto/tls"
 
     "golang.org/x/crypto/acme/autocert"
+    // "golang.org/x/crypto/acme"
 )
 
 func main(){
     log.Printf("Server starting")
+
+    // acmeClient := &acme.Client{DirectoryURL: "https://acme-staging-v02.api.letsencrypt.org/directory"}
+
+    certManager := autocert.Manager{
+        Prompt: autocert.AcceptTOS,
+        HostPolicy: autocert.HostWhitelist("magic.jonrafkind.com"),
+        // Client: acmeClient,
+        Cache: autocert.DirCache("certs"),
+    }
+
+    http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+        fmt.Fprintf(writer, "Hello, World!")
+    })
+
+    server := &http.Server{
+        Addr: ":5000",
+        TLSConfig: &tls.Config{
+            GetCertificate: certManager.GetCertificate,
+            MinVersion: tls.VersionTLS12,
+        },
+    }
+
+    go http.ListenAndServe(":5001", certManager.HTTPHandler(nil))
+
+    log.Fatal(server.ListenAndServeTLS("", ""))
 }
