28323ce542893a270838b9449dfb102d967c412f
Jon Rafkind <jon@rafkind.com>
2024-12-24 19:40:58 -0800

    add test for haste

--- game/magic/combat/combat_test.go
+++ game/magic/combat/combat_test.go
@@ -140,9 +140,59 @@ func TestBasicCombat(test *testing.T){
     }
     combat.Observer.AddObserver(observer)
 
+    // even if both units kill each other, they both get to attack
     combat.meleeAttack(defendingArmy.Units[0], attackingArmy.Units[0])
 
     if !attackerMelee || !defenderMelee {
         test.Errorf("Error: attacker and defender should have both attacked")
+    }
+}
+
+func TestBasicHaste(test *testing.T){
+    defendingArmy := &Army{
+    }
+
+    attackingArmy := &Army{
+    }
+
+    defender := units.MakeOverworldUnit(units.LizardSpearmen)
+    attacker := units.MakeOverworldUnit(units.LizardSpearmen)
+
+    defendingArmy.AddUnit(defender)
+    attackingArmy.AddUnit(attacker)
+
+    attacker.AddEnchantment(data.UnitEnchantmentHaste)
+
+    combat := &CombatScreen{
+        SelectedUnit: nil,
+        Tiles: makeTiles(5, 5, CombatLandscapeGrass, data.PlaneArcanus, ZoneType{}),
+        Turn: TeamDefender,
+        DefendingArmy: defendingArmy,
+        AttackingArmy: attackingArmy,
+    }
+
+    attackerMelee := 0
+    defenderMelee := 0
+
+    observer := &TestObserver{
+        Melee: func(meleeAttacker *ArmyUnit, meleeDefender *ArmyUnit, damageRoll int, defenderDamage int){
+            if attackingArmy.Units[0] == meleeAttacker {
+                attackerMelee += 1
+            } else if defendingArmy.Units[0] == meleeAttacker {
+                defenderMelee += 1
+            }
+        },
+    }
+    combat.Observer.AddObserver(observer)
+
+    // attacker should get to attack twice
+    combat.meleeAttack(defendingArmy.Units[0], attackingArmy.Units[0])
+
+    if attackerMelee != 2 {
+        test.Errorf("Error: attacker should have attacked twice")
+    }
+
+    if defenderMelee != 1 {
+        test.Errorf("Error: defender should have attacked once")
     }
 }
