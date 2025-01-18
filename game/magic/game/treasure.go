4024b4faf67eaac5e93ba13fb879611d2c8a53e3
Jon Rafkind <jon@rafkind.com>
2025-01-17 20:45:54 -0800

    put fields into treasure structs

--- game/magic/game/treasure.go
+++ game/magic/game/treasure.go
@@ -1,27 +1,42 @@
-package main
+package game
+
+import (
+    "github.com/kazzmir/master-of-magic/game/magic/artifact"
+    "github.com/kazzmir/master-of-magic/game/magic/spellbook"
+    "github.com/kazzmir/master-of-magic/game/magic/setup"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+)
 
 type TreasureItem interface {
 }
 
 type TreasureGold struct {
+    Amount int
 }
 
 type TreasureMana struct {
+    Amount int
 }
 
 type TreasureMagicalItem struct {
+    Artifact artifact.Artifact
 }
 
 type TreasurePrisonerHero struct {
+    Name string
 }
 
 type TreasureSpell struct {
+    Spell spellbook.Spell
 }
 
+// always gives one spellbook of the specified magic type
 type TreasureSpellbook struct {
+    Magic data.MagicType
 }
 
 type TreasureRetort struct {
+    Retort setup.WizardAbility
 }
 
 type Treasure struct {
