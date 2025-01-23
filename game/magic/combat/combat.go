de4814cefa32a9751c2724578ace9f902953d71e
Jon Rafkind <jon@rafkind.com>
2025-01-23 10:26:17 -0800

    fix unit rendering

--- game/magic/combat/combat.go
+++ game/magic/combat/combat.go
@@ -85,12 +85,12 @@ func combatPoints(count int) []image.Point {
 
 func RenderCombatUnit(screen *ebiten.Image, use *ebiten.Image, options ebiten.DrawImageOptions, count int, enchantment data.UnitEnchantment, timeCounter uint64, imageCache *util.ImageCache){
     // the ground is always 6 pixels above the bottom of the unit image
-    groundHeight := float64(6)
+    groundHeight := float64(6 * data.ScreenScale)
 
     geoM := options.GeoM
     for _, point := range combatPoints(count) {
         options.GeoM.Reset()
-        options.GeoM.Translate(float64(point.X), float64(point.Y))
+        options.GeoM.Translate(float64(point.X * data.ScreenScale), float64(point.Y * data.ScreenScale))
         options.GeoM.Translate(-float64(use.Bounds().Dx() / 2), -float64(use.Bounds().Dy()) + groundHeight)
 
         options.GeoM.Concat(geoM)
