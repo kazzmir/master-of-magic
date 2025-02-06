c23f22e2cf4dd875e0e65f7b3795e9a3823b5cba
Marc Sommerhalder <msom@gmx.ch>
2025-02-06 05:32:54 +0100

    adds famine spell

--- test/overworld/main.go
+++ test/overworld/main.go
@@ -994,6 +994,7 @@ func createScenario13(cache *lbx.LbxCache) *gamelib.Game {
     player.KnownSpells.AddSpell(allSpells.FindByName("Prosperity"))
     player.KnownSpells.AddSpell(allSpells.FindByName("Inspirations"))
     player.KnownSpells.AddSpell(allSpells.FindByName("Cursed Lands"))
+    player.KnownSpells.AddSpell(allSpells.FindByName("Famine"))
     player.KnownSpells.AddSpell(allSpells.FindByName("Nature Awareness"))
     player.KnownSpells.AddSpell(allSpells.FindByName("Change Terrain"))
     player.KnownSpells.AddSpell(allSpells.FindByName("Transmute"))
