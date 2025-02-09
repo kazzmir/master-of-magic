115fc570f9b1fac95a23d11a0443e10325a2e816
Marc Sommerhalder <msom@users.noreply.github.com>
2025-02-09 07:47:08 +0100

    city: remove city if shrunk to zero (#302)
    * city: remove city if shrunk to zero
    
    * add CityEventCityAbandoned

--- test/overworld/main.go
+++ test/overworld/main.go
@@ -354,6 +354,7 @@ func createScenario4(cache *lbx.LbxCache) *gamelib.Game {
     return game
 }
 
+// city growth message
 func createScenario5(cache *lbx.LbxCache) *gamelib.Game {
     log.Printf("Running scenario 5")
     wizard := setup.WizardCustom{
@@ -3397,6 +3398,48 @@ func createScenario40(cache *lbx.LbxCache) *gamelib.Game {
     return game
 }
 
+// shrink city to zero
+func createScenario41(cache *lbx.LbxCache) *gamelib.Game {
+    log.Printf("Running scenario 41")
+    wizard := setup.WizardCustom{
+        Name: "bob",
+        Banner: data.BannerRed,
+        Race: data.RaceTroll,
+        Abilities: []setup.WizardAbility{},
+        Books: []data.WizardBook{},
+    }
+
+    game := gamelib.MakeGame(cache, setup.NewGameSettings{
+        Magic: data.MagicSettingNormal,
+        Difficulty: data.DifficultyAverage,
+    })
+
+    game.Plane = data.PlaneArcanus
+
+    player := game.AddPlayer(wizard, true)
+
+    x, y, _ := game.FindValidCityLocation(game.Plane)
+
+    city := citylib.MakeCity("Erfurt", x, y, player.Wizard.Race, player.Wizard.Banner, player.TaxRate, game.BuildingInfo, game.CurrentMap(), game)
+    city.Population = 1000
+    city.Plane = data.PlaneArcanus
+    city.Banner = wizard.Banner
+    city.ProducingUnit = units.TrollSettlers
+    city.Production = float32(city.ProducingUnit.ProductionCost)
+    city.Farmers = 1
+    city.ResetCitizens(nil)
+
+    player.AddCity(city)
+    player.Gold = 1000
+    player.Mana = 1000
+
+    player.LiftFog(x, y, 4, data.PlaneArcanus)
+
+    game.Camera.Center(x, y)
+
+    return game
+}
+
 func NewEngine(scenario int) (*Engine, error) {
     cache := lbx.AutoCache()
 
@@ -3443,6 +3486,7 @@ func NewEngine(scenario int) (*Engine, error) {
         case 38: game = createScenario38(cache)
         case 39: game = createScenario39(cache)
         case 40: game = createScenario40(cache)
+        case 41: game = createScenario41(cache)
         default: game = createScenario1(cache)
     }
 
