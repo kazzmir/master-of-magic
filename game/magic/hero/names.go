70f3f7bc9969049743feb23e81ff6331682bca84
Jon Rafkind <jon@rafkind.com>
2025-02-17 21:50:43 -0800

    start file to read hero names

---
+++ game/magic/hero/names.go
@@ -0,0 +1,11 @@
+package hero
+
+// read the names of the heroes per wizard from names.lbx
+// each wizarc has their own set of heroes, and each wizard gives a unique name to their hero
+
+import (
+    "github.com/kazzmir/master-of-magic/lib/lbx"
+)
+
+func ReadNames(cache *lbx.LbxCache) {
+}
