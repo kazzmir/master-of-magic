6bff4be37ecce533fb59c52abc849cf60467ed51
Jon Rafkind <jon@rafkind.com>
2025-02-21 09:42:11 -0800

    test disenchant city spell on own city

--- test/overworld/main.go
+++ test/overworld/main.go
@@ -4104,6 +4104,8 @@ func createScenario47(cache *lbx.LbxCache) *gamelib.Game {
 
     player.LiftFog(x2, y2, 4, city3.Plane)
 
+    city.AddEnchantment(data.CityEnchantmentFamine, enemy.GetBanner())
+
     return game
 }
 
