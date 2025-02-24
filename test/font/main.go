39679c5926c9fbeadc061bb6946a62b2d92ef70c
Jon Rafkind <jon@rafkind.com>
2025-02-24 09:50:05 -0800

    print font with outline

--- test/font/main.go
+++ test/font/main.go
@@ -7,7 +7,9 @@ import (
     "github.com/kazzmir/master-of-magic/lib/lbx"
     // "github.com/kazzmir/master-of-magic/lib/font"
     "github.com/kazzmir/master-of-magic/game/magic/data"
+    "github.com/kazzmir/master-of-magic/game/magic/util"
     "github.com/kazzmir/master-of-magic/game/magic/fonts"
+    "github.com/kazzmir/master-of-magic/game/magic/shaders"
 
     "github.com/hajimehoshi/ebiten/v2"
     "github.com/hajimehoshi/ebiten/v2/inpututil"
@@ -15,6 +17,7 @@ import (
 
 type Engine struct {
     LbxCache *lbx.LbxCache
+    ImageCache util.ImageCache
     VaultFonts *fonts.VaultFonts
 }
 
@@ -25,6 +28,7 @@ func NewEngine() (*Engine, error) {
 
     return &Engine{
         LbxCache: cache,
+        ImageCache: util.MakeImageCache(cache),
         VaultFonts: vaultFonts,
     }, nil
 }
@@ -44,7 +48,13 @@ func (engine *Engine) Update() error {
 
 func (engine *Engine) Draw(screen *ebiten.Image) {
     screen.Fill(color.RGBA{R: 80, G: 80, B: 80, A: 255})
-    engine.VaultFonts.ItemName.Print(screen, 10, 10, 4, ebiten.ColorScale{}, "This is a test of font outlines")
+
+    shader, err := engine.ImageCache.GetShader(shaders.ShaderEdgeGlow)
+    if err != nil {
+        log.Printf("Error getting shader: %v", err)
+    } else {
+        engine.VaultFonts.ItemName.PrintOutline(screen, shader, 10, 10, 4, ebiten.ColorScale{}, "This is a test of font outlines")
+    }
 }
 
 func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
