4205094fa93fc98e51f7840a014b72d0a84ad0f1
Jon Rafkind <jon@rafkind.com>
2025-02-24 09:41:32 -0800

    print some text

---
+++ test/font/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+    "log"
+    "image/color"
+
+    "github.com/kazzmir/master-of-magic/lib/lbx"
+    // "github.com/kazzmir/master-of-magic/lib/font"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+    "github.com/kazzmir/master-of-magic/game/magic/fonts"
+
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Engine struct {
+    LbxCache *lbx.LbxCache
+    VaultFonts *fonts.VaultFonts
+}
+
+func NewEngine() (*Engine, error) {
+    cache := lbx.AutoCache()
+
+    vaultFonts := fonts.MakeVaultFonts(cache)
+
+    return &Engine{
+        LbxCache: cache,
+        VaultFonts: vaultFonts,
+    }, nil
+}
+
+func (engine *Engine) Update() error {
+
+    keys := inpututil.AppendJustPressedKeys(nil)
+
+    for _, key := range keys {
+        if key == ebiten.KeyEscape || key == ebiten.KeyCapsLock {
+            return ebiten.Termination
+        }
+    }
+
+    return nil
+}
+
+func (engine *Engine) Draw(screen *ebiten.Image) {
+    screen.Fill(color.RGBA{R: 80, G: 80, B: 80, A: 255})
+    engine.VaultFonts.ItemName.Print(screen, 10, 10, 4, ebiten.ColorScale{}, "This is a test of font outlines")
+}
+
+func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return data.ScreenWidth, data.ScreenHeight
+}
+
+func main(){
+    log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
+
+    ebiten.SetWindowSize(data.ScreenWidth * 2, data.ScreenHeight * 2)
+    ebiten.SetWindowTitle("intro")
+    ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+    engine, err := NewEngine()
+
+    if err != nil {
+        log.Printf("Error: unable to load engine: %v", err)
+        return
+    }
+
+    err = ebiten.RunGame(engine)
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
+}
