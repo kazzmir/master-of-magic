49e1aa0285d2b3a5edd17c5ccacc9070dcd708c1
Jon Rafkind <jon@rafkind.com>
2025-02-24 22:17:15 -0800

    implement cache

--- game/magic/audio/cache.go
+++ game/magic/audio/cache.go
@@ -2,7 +2,12 @@ package audio
 
 import (
     "github.com/kazzmir/master-of-magic/lib/lbx"
+    audiolib "github.com/hajimehoshi/ebiten/v2/audio"
 )
+
+const CombatLbxPath = "cmbtsnd.lbx"
+const NewSoundLbxPath = "newsound.lbx"
+const SoundLbxPath = "soundfx.lbx"
 
 type cacheKey struct {
     lbxPath string
@@ -19,4 +24,37 @@ func MakeAudioCache(cache *lbx.LbxCache) *AudioCache {
         data: make(map[cacheKey]MakePlayerFunc),
         cache: cache,
     }
+}
+
+func (cache *AudioCache) getSound(lbxPath string, index int) (*audiolib.Player, error) {
+    key := cacheKey{lbxPath: lbxPath, index: index}
+    if maker, ok := cache.data[key]; ok {
+        return maker(), nil
+    }
+
+    soundLbx, err := cache.cache.GetLbxFile(lbxPath)
+    if err != nil {
+        return nil, err
+    }
+
+    maker, err := GetSoundMaker(soundLbx, index)
+    if err != nil {
+        return nil, err
+    }
+
+    cache.data[key] = maker
+
+    return maker(), nil
+}
+
+func (cache *AudioCache) GetCombatSound(index int) (*audiolib.Player, error) {
+    return cache.getSound(CombatLbxPath, index)
+}
+
+func (cache *AudioCache) GetNewSound(index int) (*audiolib.Player, error) {
+    return cache.getSound(NewSoundLbxPath, index)
+}
+
+func (cache *AudioCache) GetSound(index int) (*audiolib.Player, error) {
+    return cache.getSound(SoundLbxPath, index)
 }
