e6978b9d0ffeaacdd938e230287d6c8c298b1fbe
Jon Rafkind <jon@rafkind.com>
2025-02-28 23:03:03 -0800

    for overworld casting, include hero caster ability

--- game/magic/magicview/view.go
+++ game/magic/magicview/view.go
@@ -768,7 +768,7 @@ func (magic *MagicScreen) MakeUI(player *playerlib.Player, enemies []*playerlib.
         Draw: func(element *uilib.UIElement, screen *ebiten.Image){
             // vector.StrokeRect(screen, float32(spellCastUIRect.Min.X), float32(spellCastUIRect.Min.Y), float32(spellCastUIRect.Dx()), float32(spellCastUIRect.Dy()), 1, color.RGBA{R: 0xff, G: 0x0, B: 0x0, A: 0xff}, false)
 
-            fonts.SmallerFont.PrintOptions(screen, float64(5 * data.ScreenScale), float64(176 * data.ScreenScale), float64(data.ScreenScale), ebiten.ColorScale{}, leftShadow, fmt.Sprintf("Casting Skill: %v(%v)", player.RemainingCastingSkill, player.ComputeCastingSkill()))
+            fonts.SmallerFont.PrintOptions(screen, float64(5 * data.ScreenScale), float64(176 * data.ScreenScale), float64(data.ScreenScale), ebiten.ColorScale{}, leftShadow, fmt.Sprintf("Casting Skill: %v(%v)", player.ComputeOverworldCastingSkill(), player.ComputeCastingSkill()))
             fonts.SmallerFont.PrintOptions(screen, float64(5 * data.ScreenScale), float64(183 * data.ScreenScale), float64(data.ScreenScale), ebiten.ColorScale{}, leftShadow, fmt.Sprintf("Magic Reserve: %v", player.Mana))
             fonts.SmallerFont.PrintOptions(screen, float64(5 * data.ScreenScale), float64(190 * data.ScreenScale), float64(data.ScreenScale), ebiten.ColorScale{}, leftShadow, fmt.Sprintf("Power Base: %v", magic.Power))
         },
