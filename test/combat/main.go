0fa6c52b9c8bf9aee39ef378ec30fff708dbfe7f
Jon Rafkind <jon@rafkind.com>
2025-03-11 11:30:53 -0700

    test raise dead on fantastic unit

--- test/combat/main.go
+++ test/combat/main.go
@@ -668,7 +668,7 @@ func makeScenario8(cache *lbx.LbxCache) *combat.CombatScreen {
                        "Creature Binding", "Mind Storm",
                        "Fire Bolt", "Ice Bolt", "Star Fires", "Dispel Evil", "Life Drain",
                        "Holy Word", "Cracks Call", "Banish", "Disintegrate", "Warp Wood", "Death Spell",
-                       "Word of Death", "Dispel Magic True", "Web", "Petrify",
+                       "Word of Death", "Dispel Magic True", "Web", "Petrify", "Raise Dead",
                    }
 
 
