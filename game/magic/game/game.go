bd4075785d50c85c89fe5f3a594a6816a6dc82cd
Jon Rafkind <jon@rafkind.com>
2025-03-19 11:29:19 -0700

    move damage stuff to its own file

--- game/magic/game/game.go
+++ game/magic/game/game.go
@@ -7601,60 +7601,6 @@ func (game *Game) DoRandomEvents() {
     game.RandomEvents = keep
 }
 
-// an implementation of combat/UnitDamage that wraps a StackUnit for the purpose of applying damage to a StackUnit
-type UnitDamageWrapper struct {
-    Unit units.StackUnit
-}
-
-func (wrapper *UnitDamageWrapper) GetMaxHealth() int {
-    return wrapper.Unit.GetMaxHealth()
-}
-
-func (wrapper *UnitDamageWrapper) GetHealth() int {
-    return wrapper.Unit.GetHealth()
-}
-
-func (wrapper *UnitDamageWrapper) GetCount() int {
-    return wrapper.Unit.GetCount()
-}
-
-func (wrapper *UnitDamageWrapper) GetDefense() int {
-    return wrapper.Unit.GetDefense()
-}
-
-func (wrapper *UnitDamageWrapper) HasAbility(ability data.AbilityType) bool {
-    return wrapper.Unit.HasAbility(ability)
-}
-
-func (wrapper *UnitDamageWrapper) HasEnchantment(enchantment data.UnitEnchantment) bool {
-    return wrapper.Unit.HasEnchantment(enchantment)
-}
-
-func (wrapper *UnitDamageWrapper) IsAsleep() bool {
-    return false
-}
-
-func (wrapper *UnitDamageWrapper) TakeDamage(damage int, damageType combat.DamageType) {
-    wrapper.Unit.AdjustHealth(-damage)
-}
-
-func (wrapper *UnitDamageWrapper) ToDefend(modifiers combat.DamageModifiers) int {
-    return 30
-}
-
-func (wrapper *UnitDamageWrapper) ReduceInvulnerability(damage int) int {
-    if wrapper.Unit.HasEnchantment(data.UnitEnchantmentInvulnerability) {
-        return max(0, damage - 2)
-    }
-
-    return damage
-}
-
-func (wrapper *UnitDamageWrapper) Figures() int {
-    health_per_figure := float64(wrapper.GetMaxHealth()) / float64(wrapper.GetCount())
-    return int(math.Ceil(float64(wrapper.GetHealth()) / health_per_figure))
-}
-
 func (game *Game) doChaosRift() {
     for _, city := range game.AllCities() {
         if city.HasEnchantment(data.CityEnchantmentChaosRift) {
