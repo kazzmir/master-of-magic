8ec5847a268ee365acc73d91c17dc6cc7de7bce7
Jon Rafkind <jon@rafkind.com>
2025-03-28 19:50:02 -0700

    show animations when casting

--- game/magic/mastery/mastery.go
+++ game/magic/mastery/mastery.go
@@ -1,9 +1,61 @@
 package mastery
 
 import (
+    "github.com/kazzmir/master-of-magic/game/magic/util"
+    "github.com/kazzmir/master-of-magic/game/magic/scale"
     "github.com/kazzmir/master-of-magic/lib/coroutine"
     "github.com/kazzmir/master-of-magic/lib/lbx"
+
+    "github.com/hajimehoshi/ebiten/v2"
 )
 
-func ShowSpellOfMasteryScreen(yield coroutine.YieldFunc, cache *lbx.LbxCache, wizard string) {
+func ShowSpellOfMasteryScreen(cache *lbx.LbxCache, wizard string) (coroutine.AcceptYieldFunc, func (*ebiten.Image)) {
+    // play animations spellscr.lbx 67, 68, 69, 70 in order
+    // overlay text "$wizard has started casting the Spell of Mastery"
+
+    imageCache := util.MakeImageCache(cache)
+
+    order := []int{67, 68, 69, 70}
+    index := 0
+
+    images1, _ := imageCache.GetImages("spellscr.lbx", 67)
+    animation := util.MakeAnimation(images1, false)
+
+    var logic coroutine.AcceptYieldFunc
+
+    var counter uint64 = 0
+
+    logic = func (yield coroutine.YieldFunc) error {
+        for !animation.Done() {
+            counter += 1
+            if yield() != nil {
+                return nil
+            }
+
+            if counter % 10 == 0 {
+                if !animation.Next() {
+                    index += 1
+                    if index < len(order) {
+                        images, _ := imageCache.GetImages("spellscr.lbx", order[index])
+                        animation = util.MakeAnimation(images, false)
+                    }
+                }
+            }
+        }
+
+        return nil
+    }
+
+    draw := func (screen *ebiten.Image) {
+        var options ebiten.DrawImageOptions
+        scale.DrawScaled(screen, animation.Frame(), &options)
+    }
+
+    return logic, draw
+}
+
+func CastSpellOfMastery() {
+    // show wizlab with wizard standing there, vortex animation (splmastr.lbx 29-31) with wizard faces flying around (spelllose.lbx 0-13)
+
+    // then show wizard from win.lbz 3-16, background win.lbz 0, hands 17-22, world animation 2, and some text about being the master of magic. then show score screen
 }
---
+++ test/mastery/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+    "log"
+    "strconv"
+    "os"
+    "math/rand/v2"
+
+    "github.com/kazzmir/master-of-magic/lib/lbx"
+    "github.com/kazzmir/master-of-magic/lib/coroutine"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+    "github.com/kazzmir/master-of-magic/game/magic/mastery"
+    "github.com/kazzmir/master-of-magic/game/magic/audio"
+    "github.com/kazzmir/master-of-magic/game/magic/scale"
+    playerlib "github.com/kazzmir/master-of-magic/game/magic/player"
+    "github.com/kazzmir/master-of-magic/game/magic/setup"
+
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Engine struct {
+    LbxCache *lbx.LbxCache
+    Coroutine *coroutine.Coroutine
+    DrawScene func (*ebiten.Image)
+}
+
+func randomWizard() data.WizardBase {
+    choices := []data.WizardBase{
+        data.WizardMerlin, data.WizardRaven, data.WizardSharee,
+        data.WizardLoPan, data.WizardJafar, data.WizardOberic,
+        data.WizardRjak, data.WizardSssra, data.WizardTauron,
+        data.WizardFreya, data.WizardHorus, data.WizardAriel,
+        data.WizardTlaloc, data.WizardKali,
+    }
+
+    return choices[rand.IntN(len(choices))]
+}
+
+func NewEngine(scenario int) (*Engine, error) {
+    cache := lbx.AutoCache()
+
+    player1 := playerlib.Player{
+        Wizard: setup.WizardCustom{
+            Base: randomWizard(),
+            Name: "bob",
+        },
+    }
+
+    /*
+    player2 := playerlib.Player{
+        Wizard: setup.WizardCustom{
+            Base: randomWizard(),
+            Name: "Kali",
+        },
+    }
+    */
+
+    logic, draw := mastery.ShowSpellOfMasteryScreen(cache, player1.Wizard.Name)
+
+    return &Engine{
+        LbxCache: cache,
+        DrawScene: draw,
+        Coroutine: coroutine.MakeCoroutine(logic),
+    }, nil
+}
+
+func (engine *Engine) Update() error {
+
+    keys := make([]ebiten.Key, 0)
+    keys = inpututil.AppendJustPressedKeys(keys)
+
+    for _, key := range keys {
+        if key == ebiten.KeyEscape || key == ebiten.KeyCapsLock {
+            return ebiten.Termination
+        }
+    }
+
+    if engine.Coroutine.Run() != nil {
+        return ebiten.Termination
+    }
+
+    return nil
+}
+
+func (engine *Engine) Draw(screen *ebiten.Image) {
+    engine.DrawScene(screen)
+}
+
+func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return scale.Scale2(data.ScreenWidth, data.ScreenHeight)
+}
+
+func main(){
+    log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
+
+    monitorWidth, _ := ebiten.Monitor().Size()
+    size := monitorWidth / 390
+
+    ebiten.SetWindowSize(data.ScreenWidth * size, data.ScreenHeight * size)
+    ebiten.SetWindowTitle("banish")
+    ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+    audio.Initialize()
+
+    scenario := 1
+
+    if len(os.Args) >= 2 {
+        x, err := strconv.Atoi(os.Args[1])
+        if err != nil {
+            log.Fatalf("Error with scenario: %v", err)
+        }
+
+        scenario = x
+    }
+
+    engine, err := NewEngine(scenario)
+
+    if err != nil {
+        log.Printf("Error: unable to load engine: %v", err)
+        return
+    }
+
+    err = ebiten.RunGame(engine)
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
+}
