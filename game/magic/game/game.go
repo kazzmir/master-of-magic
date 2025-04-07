ce363faa80dced8c58c685e6cc74bf518ebf9ef1
Jon Rafkind <jon@rafkind.com>
2025-04-07 00:44:53 -0700

    move city stack info to player

--- game/magic/game/game.go
+++ game/magic/game/game.go
@@ -5805,73 +5805,8 @@ func (game *Game) IsGlobalEnchantmentActive(enchantment data.Enchantment) bool {
     })
 }
 
-// stores information about every stack and city for fast lookups
-// Warning: do not store this information for long periods of time as it will become out of date
-type CityStackInfo struct {
-    ArcanusStacks map[image.Point]*playerlib.UnitStack
-    MyrrorStacks map[image.Point]*playerlib.UnitStack
-    ArcanusCities map[image.Point]*citylib.City
-    MyrrorCities map[image.Point]*citylib.City
-}
-
-func (info CityStackInfo) FindStack(x int, y int, plane data.Plane) *playerlib.UnitStack {
-    var use map[image.Point]*playerlib.UnitStack
-
-    switch plane {
-        case data.PlaneArcanus: use = info.ArcanusStacks
-        case data.PlaneMyrror: use = info.MyrrorStacks
-    }
-
-    stack, ok := use[image.Pt(x, y)]
-    if ok {
-        return stack
-    }
-
-    return nil
-}
-
-func (info CityStackInfo) FindCity(x int, y int, plane data.Plane) *citylib.City {
-    var use map[image.Point]*citylib.City
-
-    switch plane {
-        case data.PlaneArcanus: use = info.ArcanusCities
-        case data.PlaneMyrror: use = info.MyrrorCities
-    }
-
-    city, ok := use[image.Pt(x, y)]
-    if ok {
-        return city
-    }
-
-    return nil
-}
-
-func (info CityStackInfo) ContainsEnemy(x int, y int, plane data.Plane, player *playerlib.Player) bool {
-    stack := info.FindStack(x, y, plane)
-    if stack != nil && stack.GetBanner() != player.GetBanner() {
-        return true
-    }
-
-    city := info.FindCity(x, y, plane)
-    if city != nil && city.GetBanner() != player.GetBanner() {
-        return true
-    }
-
-    return false
-}
-
-func (info CityStackInfo) FindFriendlyStack(x int, y int, plane data.Plane, player *playerlib.Player) *playerlib.UnitStack {
-    stack := info.FindStack(x, y, plane)
-
-    if stack != nil && stack.GetBanner() == player.GetBanner() {
-        return stack
-    }
-
-    return nil
-}
-
-func (game *Game) ComputeCityStackInfo() CityStackInfo {
-    out := CityStackInfo{
+func (game *Game) ComputeCityStackInfo() playerlib.CityStackInfo {
+    out := playerlib.CityStackInfo{
         ArcanusStacks: make(map[image.Point]*playerlib.UnitStack),
         MyrrorStacks: make(map[image.Point]*playerlib.UnitStack),
         ArcanusCities: make(map[image.Point]*citylib.City),
