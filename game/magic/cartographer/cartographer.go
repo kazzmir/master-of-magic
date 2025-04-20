26df3680bd02e1e8d42b8d958955c477138a2e41
Jon Rafkind <jon@rafkind.com>
2025-04-20 08:44:29 -0700

    start cartographer

---
+++ game/magic/cartographer/cartographer.go
@@ -0,0 +1,48 @@
+package cartographer
+
+import (
+    "github.com/kazzmir/master-of-magic/lib/coroutine"
+    "github.com/kazzmir/master-of-magic/lib/lbx"
+    "github.com/kazzmir/master-of-magic/game/magic/util"
+    "github.com/kazzmir/master-of-magic/game/magic/scale"
+
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+func MakeCartographer(cache *lbx.LbxCache) (func(coroutine.YieldFunc), func (*ebiten.Image)) {
+
+    imageCache := util.MakeImageCache(cache)
+
+    counter := uint64(0)
+
+    getAlpha := util.MakeFadeIn(7, &counter)
+
+    logic := func (yield coroutine.YieldFunc) {
+        quit := false
+        for !quit {
+            counter += 1
+            if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+                quit = true
+            }
+
+            if yield() != nil {
+                return
+            }
+        }
+
+        getAlpha = util.MakeFadeOut(7, &counter)
+        for range 7 {
+            yield()
+        }
+    }
+
+    draw := func (screen *ebiten.Image) {
+        background, _ := imageCache.GetImage("reload.lbx", 2, 0)
+        var options ebiten.DrawImageOptions
+        options.ColorScale.ScaleAlpha(getAlpha())
+        scale.DrawScaled(screen, background, &options)
+    }
+
+    return logic, draw
+}
