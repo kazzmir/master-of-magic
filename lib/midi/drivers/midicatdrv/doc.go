4a64adfda7b38a9ea4cd0ccb33049b83c535ea24
Jon Rafkind <jon@rafkind.com>
2025-04-26 14:53:15 -0700

    remove more drivers

--- lib/midi/drivers/midicatdrv/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021 Marc René Arns. All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
-
-/*
-Package midicatdrv provides a Driver to connect via the program midicat (from the tools).
-*/
-package midicatdrv
--- lib/midi/drivers/webmididrv/README.md
+++
@@ -1,9 +0,0 @@
-# webmididrv
-
-This is driver uses the js/wasm compilation target and runs in the browsers that support the webmidi standard
-(all browsers based on Chrome).
-
-## Example
-
-see the example directory
-
