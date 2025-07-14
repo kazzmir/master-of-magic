b17d0736236eaec9e04b17c5405afd98a5d3b141
Jon Rafkind <jon@rafkind.com>
2025-07-14 09:30:19 -0700

    start to convert from game to save game

---
+++ game/magic/load/convert-from.go
@@ -0,0 +1,11 @@
+package load
+
+import (
+    "fmt"
+    gamelib "github.com/kazzmir/master-of-magic/game/magic/game"
+)
+
+func CreateSaveGame(game *gamelib.Game) (*SaveGame, error) {
+
+    return nil, fmt.Errorf("CreateSaveGame not implemented yet")
+}
