67dfd8d102b3fccb89a97f70462eab717411d9fb
Jon Rafkind <jon@rafkind.com>
2025-08-21 15:05:48 -0700

    create player with a unit

--- game/arena/main.go
+++ game/arena/main.go
@@ -4,13 +4,49 @@ import (
     "log"
 
     "github.com/kazzmir/master-of-magic/lib/lbx"
-    // "github.com/kazzmir/master-of-magic/game/magic/data"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+    "github.com/kazzmir/master-of-magic/game/magic/units"
 
     "github.com/hajimehoshi/ebiten/v2"
     "github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
+/*
+ * Start with a small army (single unit?), fight a battle. If you win then you get money/score that you can use to buy more units and spells.
+ *  1. start game, pick a wizard portrait, name, etc
+ *  2. pick an army from a small set of units
+ *  3. fight a battle against an equivalent foe
+ *  4. use money to buy more units and spells
+ *  5. repeat from step 3
+ */
+
+type GameMode int
+
+const (
+    GameModeUI GameMode = iota
+    GameModeBattle
+)
+
+type Player struct {
+    Name string
+    Money uint64
+
+    Units []units.StackUnit
+}
+
+func MakePlayer() *Player {
+    return &Player{
+        Name: "Player",
+        Money: 1000,
+        Units: []units.StackUnit{
+            units.MakeOverworldUnitFromUnit(units.LizardSwordsmen, 0, 0, data.PlaneArcanus, data.BannerGreen, &units.NoExperienceInfo{}, &units.NoEnchantments{}),
+        },
+    }
+}
+
 type Engine struct {
+    GameMode GameMode
+    Player *Player
 }
 
 func (engine *Engine) Update() error {
@@ -26,15 +62,35 @@ func (engine *Engine) Update() error {
     return nil
 }
 
+func (engine *Engine) DrawUI(screen *ebiten.Image) {
+}
+
+func (engine *Engine) DrawBattle(screen *ebiten.Image) {
+}
+
 func (engine *Engine) Draw(screen *ebiten.Image) {
+    switch engine.GameMode {
+        case GameModeUI:
+            engine.DrawUI(screen)
+        case GameModeBattle:
+            engine.DrawBattle(screen)
+    }
 }
 
 func (engine *Engine) Layout(outsideWidth, outsideHeight int) (int, int) {
+    switch engine.GameMode {
+        case GameModeUI: return outsideWidth, outsideHeight
+        case GameModeBattle: return data.ScreenWidth, data.ScreenHeight
+    }
+
     return outsideWidth, outsideHeight
 }
 
 func MakeEngine(cache *lbx.LbxCache) *Engine {
-    return &Engine{}
+    return &Engine{
+        GameMode: GameModeUI,
+        Player: MakePlayer(),
+    }
 }
 
 func main() {
