90fd5b24f632e8e2d39f3ab1c831a88a00318ddc
Jon Rafkind <jon@rafkind.com>
2025-10-29 21:35:49 -0700

    comment

--- game/magic/inputmanager/input_js.go
+++ game/magic/inputmanager/input_js.go
@@ -5,6 +5,8 @@ import (
     "github.com/hajimehoshi/ebiten/v2"
 )
 
+// don't support the quit key on browsers so that people don't accidentally quit, since it doesn't make a lot of sense in that case
+
 func IsQuitKey(key ebiten.Key) bool {
     return false
 }
