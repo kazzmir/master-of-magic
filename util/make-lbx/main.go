9c3a492100993325ce49a8d7683fc57f678c252d
Jon Rafkind <jon@rafkind.com>
2025-11-04 21:50:16 -0800

    try to save terrain data

--- util/make-lbx/main.go
+++ util/make-lbx/main.go
@@ -5,6 +5,7 @@ import (
     "io"
     "log"
     "fmt"
+    "flag"
     "image"
     "bufio"
     "bytes"
@@ -22,10 +23,11 @@ const (
 
 type LbxBuilder struct {
     Data [][]byte
+    TerrainData [][]byte
 }
 
 func (builder *LbxBuilder) NumData() int {
-    return len(builder.Data)
+    return len(builder.Data) + len(builder.TerrainData)
 }
 
 func (builder *LbxBuilder) AddImage(img image.Image) error {
@@ -43,7 +45,7 @@ func (builder *LbxBuilder) AddTerrain(img image.Image) error {
         return err
     }
 
-    builder.Data = append(builder.Data, encoded)
+    builder.TerrainData = append(builder.TerrainData, encoded)
     return nil
 }
 
@@ -62,6 +64,7 @@ func (builder *LbxBuilder) AddFileType(path string, lbxType LbxType) error {
                 return err
             }
             builder.Data = append(builder.Data, buf.Bytes())
+            return nil
         case LbxImage:
             img, _, err := image.Decode(bufio.NewReader(file))
             if err != nil {
@@ -80,15 +83,41 @@ func (builder *LbxBuilder) AddFileType(path string, lbxType LbxType) error {
 }
 
 func (builder *LbxBuilder) MakeLbx() *lbx.LbxFile {
-    return &lbx.LbxFile{
+    out := lbx.LbxFile{
         Data: builder.Data,
     }
+
+    if len(builder.TerrainData) > 0 {
+        var total bytes.Buffer
+        total.Write(make([]byte, 192))
+        for _, terrain := range builder.TerrainData {
+            total.Write(terrain)
+        }
+
+        out.Data = append(out.Data, total.Bytes())
+    }
+
+    return &out
 }
 
 func main() {
     log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
 
-    files := os.Args[1:]
+    lbxType := LbxRaw
+
+    useImage := flag.Bool("image", false, "Treat input files as images")
+    useTerrain := flag.Bool("terrain", false, "Treat input files as terrain images")
+    flag.Parse()
+
+    if *useImage {
+        lbxType = LbxImage
+    }
+
+    if *useTerrain {
+        lbxType = LbxTerrain
+    }
+
+    files := os.Args[1 + flag.NFlag():]
 
     if len(files) == 0 {
         log.Fatal("No input files provided")
@@ -99,7 +128,7 @@ func main() {
     var builder LbxBuilder
 
     for _, filePath := range files {
-        err := builder.AddFileType(filePath, LbxImage)
+        err := builder.AddFileType(filePath, lbxType)
         if err != nil {
             log.Printf("Error processing file %s: %v\n", filePath, err)
         }
