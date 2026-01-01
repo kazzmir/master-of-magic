d4d469aa7d467569d2036c3e2e341d135bee9b31
Jon Rafkind <jon@rafkind.com>
2026-01-01 12:53:29 -0800

    serialize hero data

--- game/magic/hero/serialize.go
+++ game/magic/hero/serialize.go
@@ -1,9 +1,47 @@
 package hero
 
+import (
+    "github.com/kazzmir/master-of-magic/game/magic/units"
+    "github.com/kazzmir/master-of-magic/game/magic/data"
+)
+
+type SerializedAbility struct {
+    Type data.AbilityType `json:"type"`
+    Value float32 `json:"value"`
+}
+
 type SerializedHeroUnit struct {
+    Base units.SerializedOverworldUnit `json:"base"`
+    HeroType HeroType `json:"hero_type"`
+    Name string `json:"name"`
+    Status HeroStatus `json:"status"`
+
+    // set at start of game
+    Abilities []SerializedAbility `json:"abilities"`
+
+    // Equipment [3]*artifact.Artifact
+}
+
+func serializeAbilities(abilities map[data.AbilityType]data.Ability) []SerializedAbility {
+    serialized := make([]SerializedAbility, 0, len(abilities))
+    for abilityType, ability := range abilities {
+        serialized = append(serialized, SerializedAbility{
+            Type: abilityType,
+            Value: ability.Value,
+        })
+    }
+    return serialized
 }
 
 func SerializeHero(hero *Hero) SerializedHeroUnit {
-    return SerializedHeroUnit{}
+
+    return SerializedHeroUnit{
+        Base: units.SerializeOverworldUnit(hero.OverworldUnit),
+        HeroType: hero.HeroType,
+        Name: hero.Name,
+        Status: hero.Status,
+        Abilities: serializeAbilities(hero.Abilities),
+        // Equipment: hero.Equipment,
+    }
 }
 
