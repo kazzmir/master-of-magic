9e8f2d45ce8a701dda2416b9ee31dd91c5d9b677
Jon Rafkind <jon@rafkind.com>
2026-01-01 21:56:03 -0800

    add serialize

---
+++ game/magic/serialize/serialize.go
@@ -0,0 +1,59 @@
+package serialize
+
+import (
+    "os"
+    "time"
+    "bufio"
+    "compress/gzip"
+    "encoding/json"
+)
+
+type SaveMetadata struct {
+    Version int `json:"version"`
+    Date time.Time `json:"date"`
+    Name string `json:"name"`
+}
+
+func LoadMetadata(path string) (SaveMetadata, bool) {
+    file, err := os.Open(path)
+    if err != nil {
+        return SaveMetadata{}, false
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    gzipReader, err := gzip.NewReader(reader)
+    if err != nil {
+        return SaveMetadata{}, false
+    }
+    defer gzipReader.Close()
+
+    var data map[string]any
+    decoder := json.NewDecoder(gzipReader)
+    err = decoder.Decode(&data)
+    if err != nil {
+        return SaveMetadata{}, false
+    }
+
+    metadata, found := data["metadata"]
+    if found {
+        metadataMap, ok := metadata.(map[string]any)
+        if ok {
+            raw, err := json.Marshal(metadataMap)
+            if err != nil {
+                return SaveMetadata{}, false
+            }
+
+            var meta SaveMetadata
+            err = json.Unmarshal(raw, &meta)
+            if err != nil {
+                return SaveMetadata{}, false
+            }
+
+            return meta, true
+        }
+    }
+
+    return SaveMetadata{}, false
+}
+
