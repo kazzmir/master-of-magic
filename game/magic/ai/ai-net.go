fdb9adf2f201754caf101e8a7fbe77ffd0da8c37
Jon Rafkind <jon@rafkind.com>
2026-06-01 22:49:38 -0700

    add more features

--- game/magic/ai/ai-net.go
+++ game/magic/ai/ai-net.go
@@ -6,6 +6,7 @@ import (
 
     "github.com/kazzmir/master-of-magic/game/magic/data"
     "github.com/kazzmir/master-of-magic/game/magic/units"
+    "github.com/kazzmir/master-of-magic/game/magic/spellbook"
     "github.com/kazzmir/master-of-magic/game/magic/pathfinding"
     "github.com/kazzmir/master-of-magic/game/magic/maplib"
     playerlib "github.com/kazzmir/master-of-magic/game/magic/player"
@@ -69,36 +70,153 @@ func countMyrrorUnits(units iter.Seq[units.StackUnit]) int {
     return countUnits(units, data.PlaneMyrror)
 }
 
-func featureExtraction(player *playerlib.Player, services playerlib.AIServices) []float64 {
-    var features []float64
+func countSettlers(units iter.Seq[units.StackUnit]) int {
+    count := 0
+    for unit := range units {
+        if unit.HasAbility(data.AbilityCreateOutpost) {
+            count += 1
+        }
+    }
+    return count
+}
 
+func totalPopulation(player *playerlib.Player) int {
+    population := 0
+    for _, city := range player.Cities {
+        population += city.Population
+    }
+
+    return population
+}
+
+func countExploredTiles(player *playerlib.Player, services playerlib.AIServices, plane data.Plane) int {
+    map_ := services.GetMap(plane)
+    count := 0
+    for x := range map_.Width() {
+        for y := range map_.Height() {
+            if player.IsExplored(x, y, plane) {
+                count += 1
+            }
+        }
+    }
+    return count
+}
+
+func countVisibleEnemyUnits(player *playerlib.Player, services playerlib.AIServices, plane data.Plane) int {
+
+    map_ := services.GetMap(plane)
+    count := 0
+    for x := range map_.Width() {
+        for y := range map_.Height() {
+            if player.IsVisible(x, y, plane) {
+                stack, owner := services.FindStack(x, y, plane)
+                if stack != nil && owner != nil && owner != player {
+                    count += stack.Size()
+                }
+            }
+        }
+    }
+
+    return count
+}
+
+func countMagicNodes(player *playerlib.Player, services playerlib.AIServices, plane data.Plane) int {
+    map_ := services.GetMap(plane)
+    return len(map_.GetMeldedNodes(player))
+}
+
+func featureExtraction(player *playerlib.Player, services playerlib.AIServices) []float64 {
     // each feature should be a normalized value in the range [0, 1] representing some aspect of the game state relevant to decision making
 
-    features = append(features, min(1, float64(services.GetTurnNumber()) / 1000))
-    features = append(features, min(1, float64(player.Gold) / 20000))
-    features = append(features, min(1, float64(player.Mana) / 20000))
-    features = append(features, min(1, float64(countArcanusCities(player)) / 30))
-    features = append(features, min(1, float64(countMyrrorCities(player)) / 30))
-    features = append(features, min(1, float64(len(player.AliveHeroes())) / 6))
-    features = append(features, min(1, float64(countArcanusUnits(player.Units()) / 100)))
-    features = append(features, min(1, float64(countMyrrorUnits(player.Units()) / 100)))
+    features := []float64{
+        float64(services.GetTurnNumber()) / 2000,
+        float64(player.Gold) / 50000,
+        float64(player.Mana) / 20000,
+        float64(countArcanusCities(player)) / 30,
+        float64(countMyrrorCities(player)) / 30,
+        float64(len(player.AliveHeroes())) / 6,
+        float64(countSettlers(player.Units()) / 100),
+        float64(countArcanusUnits(player.Units()) / 100),
+        float64(countMyrrorUnits(player.Units()) / 100),
+        float64(totalPopulation(player)) / 1000,
+        float64(player.TotalUnitUpkeepGold()) / 1000,
+        float64(player.TotalUnitUpkeepMana()) / 1000,
+        float64(player.TotalUnitUpkeepFood()) / 1000,
+        float64(countExploredTiles(player, services, data.PlaneArcanus)) / 5000,
+        float64(countExploredTiles(player, services, data.PlaneMyrror)) / 5000,
+        float64(countVisibleEnemyUnits(player, services, data.PlaneArcanus)) / 100,
+        float64(countVisibleEnemyUnits(player, services, data.PlaneMyrror)) / 100,
+        float64(countMagicNodes(player, services, data.PlaneArcanus)) / 20,
+        float64(countMagicNodes(player, services, data.PlaneMyrror)) / 20,
+        (func() float64 {
+            if player.ResearchingSpell.Valid() {
+                return float64(player.ResearchProgress) / float64(player.ResearchingSpell.ResearchCost)
+            } else {
+                return 0
+            }
+        })(),
+        (func() float64 {
+            if player.CastingSpell.Valid() {
+                spellCost := player.ComputeEffectiveSpellCost(player.CastingSpell, true)
+                return float64(player.CastingSpellProgress) / float64(spellCost)
+            } else {
+                return 0
+            }
+        })(),
+        float64(len(player.KnownSpells.GetSpellsByRarity(spellbook.SpellRarityCommon).Spells)) / 100.0,
+        float64(len(player.KnownSpells.GetSpellsByRarity(spellbook.SpellRarityUncommon).Spells)) / 100.0,
+        float64(len(player.KnownSpells.GetSpellsByRarity(spellbook.SpellRarityRare).Spells)) / 100.0,
+        float64(len(player.KnownSpells.GetSpellsByRarity(spellbook.SpellRarityVeryRare).Spells)) / 100.0,
+        (func() float64 {
+            if player.ResearchCandidateSpells.Contains(spellbook.SpellOfMastery) {
+                return 1
+            }
+            return 0
+        })(),
+        (func() float64 {
+            if player.CastingSpell.Valid() && player.CastingSpell.Name == spellbook.SpellOfMastery.Name {
+                return 1
+            }
+            return 0
+        })(),
+        (func() float64 {
+            knownPlayers := player.GetKnownPlayers()
+            count := 0
+            for _, known := range knownPlayers {
+                if !known.Defeated && !known.Banished {
+                    count += 1
+                }
+            }
 
-    if player.ResearchingSpell.Valid() {
-        features = append(features, min(1, float64(player.ResearchProgress) / float64(player.ResearchingSpell.ResearchCost)))
-    } else {
-        features = append(features, 0)
+            // max 3 opponents
+            return float64(count) / 3.0
+        })(),
+        (func() float64 {
+            if player.Banished {
+                return 1
+            }
+            return 0
+        })(),
+        float64(player.Fame) / 1000,
+        player.TaxRate.ToFloat() / 5,
+        player.PowerDistribution.Mana,
+        player.PowerDistribution.Skill,
+        player.PowerDistribution.Research,
+        float64(player.CastingSkillPower) / 100000,
     }
-    if player.CastingSpell.Valid() {
-        spellCost := player.ComputeEffectiveSpellCost(player.CastingSpell, true)
-        features = append(features, min(1, float64(player.CastingSpellProgress) / float64(spellCost)))
-    } else {
-        features = append(features, 0)
+
+    for i := range features {
+        features[i] = min(1, max(-1, features[i]))
     }
 
     return features
 }
 
 func (ai *EnemyNetAI) Update(player *playerlib.Player, services playerlib.AIServices) []playerlib.AIDecision {
+    // create feature vector from game state, feed it to neural network, get strategy probabilities
+    // use strategy probabilities to select specific actions to take this turn
+    // get reward signals, feed back into neural network for training
+
     return nil
 }
 
