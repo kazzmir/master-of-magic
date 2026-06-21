2aa1830af45bbbd3e4a123b8162adebddc45108d
sidav <sidav94@gmail.com>
2026-06-21 11:45:06 +0200

    Add shadows to gold/food/mana display.

--- game/magic/game/game.go
+++ game/magic/game/game.go
@@ -6470,8 +6470,10 @@ func (game *Game) MakeHudUI() *uilib.UI {
                     negative := options
                     negative.ColorScale = negativeScale
 
-                    negativeOptions := font.FontOptions{Justify: font.FontJustifyCenter, Options: &negative, Scale: scale.ScaleAmount}
-                    normalOptions := font.FontOptions{Justify: font.FontJustifyCenter, Options: &options, Scale: scale.ScaleAmount}
+                    negativeOptions := font.FontOptions{Justify: font.FontJustifyCenter, Options: &negative, Scale: scale.ScaleAmount,
+						DropShadow: true, ShadowColor: color.RGBA{128, 0, 0, 255}, ShadowDistance: 0.6}
+                    normalOptions := font.FontOptions{Justify: font.FontJustifyCenter, Options: &options, Scale: scale.ScaleAmount, 
+						DropShadow: true, ShadowColor: color.RGBA{96, 32, 0, 255}, ShadowDistance: 0.6}
 
                     if goldPerTurn < 0 {
                         game.Fonts.InfoFontRed.PrintOptions(screen, 278, 103, negativeOptions, fmt.Sprintf("%v Gold", goldPerTurn))
